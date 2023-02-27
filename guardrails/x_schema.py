38389e7db33d5457cb30d95a980d8adc9b326e05
Shreya Rajpal <shreya.rajpal@gmail.com>
2023-02-27 01:14:59 -0800

    functional schema loading

--- guardrails/x_schema.py
+++ guardrails/x_schema.py
@@ -9,13 +9,22 @@
 
 # from pyparsing import CaselessKeyword, Regex
 
-from typing import List, Union
+# from typing import List, Union
 
-from xml.etree import ElementTree
+# from xml.etree import ElementTree
 
-from guardrails.utils.xml_utils import validate_xml
-from guardrails.x_datatypes import DataType
-from guardrails.x_validators import Validator
+# from guardrails.utils.xml_utils import validate_xml
+# from guardrails.x_datatypes import DataType
+# from guardrails.x_validators import Validator
+
+from typing import Dict, List, Union
+import warnings
+
+from xml.etree import ElementTree as ET
+
+from guardrails.x_datatypes import registry as types_registry, DataType
+from guardrails.x_validators import types_to_validators, validators_registry, Validator
+
 
 
 class Field:
@@ -27,11 +36,17 @@ class Field:
         self.validators = validators
         self.children = {}
 
+    def __repr__(self):
+        return f"Field({self.type}, {self.validators}, {self.children})"
+
 
 class XSchema:
     # def __init__(self, schema: Dict[str, Any]):
     def __init__(self, schema):
         self.schema = schema
+
+    def __repr__(self):
+        return f"XSchema({self.schema})"
 
     @classmethod
     def from_xml(cls, xml_file: str, base_prompt: str) -> "XSchema":
@@ -40,10 +55,125 @@ class XSchema:
         with open(xml_file, "r") as f:
             xml = f.read()
 
-        tree = ElementTree.fromstring(xml)
-        parsed_xml = cls._parse_xml(tree, xml_file, base_prompt)
+        parser = ET.XMLParser(encoding="utf-8")
+        parsed_xml = ET.fromstring(xml, parser=parser)
 
         # TODO(shreya): Make this return a schema object.
-        _ = validate_xml(parsed_xml, strict=False)
+        schema = validate_xml(parsed_xml, strict=False)
 
         return cls(schema)
+
+
+def validate_xml(tree: Union[ET.ElementTree, ET.Element], strict: bool = False) -> bool:
+    """Validate parsed XML, create a prompt and a Schema object."""
+
+    if type(tree) == ET.ElementTree:
+        tree = tree.getroot()
+
+    schema = validate_element(tree, strict=strict)
+
+    return schema
+
+
+def get_formatters(element: ET.Element, strict: bool = False) -> List[Validator]:
+    """Get the formatters for an element.
+
+    Args:
+        element: The XML element.
+        strict: If True, raise an error if the element is not registered.
+
+    Returns:
+        A list of formatters.
+    """
+    registered_formatters = types_to_validators[element.tag]
+    provided_formatters = element.attrib['format'].split(';')
+
+    valid_formatters = []
+
+    for formatter in provided_formatters:
+        # Check if the formatter has any arguments.
+
+        formatter = formatter.strip()
+        print(f'formatter: {formatter}')
+
+        args = []
+        formatter_with_args = formatter.split(':')
+        if len(formatter_with_args) > 1:
+            assert len(formatter_with_args) == 2, (
+                f"Formatter {formatter} has too many arguments.")
+            formatter, args = formatter_with_args
+            formatter = formatter.strip()
+            args = [x.strip() for x in args.strip().split(' ')]
+
+        if formatter not in registered_formatters:
+            if strict:
+                raise ValueError(
+                    f"Formatter {formatter} is not valid for element {element.tag}.")
+            else:
+                warnings.warn(
+                    f"Formatter {formatter} is not valid for element {element.tag}.")
+            continue
+
+        # See if the formatter has an associated on_fail method.
+        on_fail = None
+        on_fail_attr_name = f'on-fail-{formatter}'
+        if on_fail_attr_name in element.attrib:
+            on_fail = element.attrib[on_fail_attr_name]
+            # TODO(shreya): Load the on_fail method.
+            # This method should be loaded from an optional script given at the
+            # beginning of a gxml file.
+
+        formatter = validators_registry[formatter]
+        valid_formatters.append(formatter(*args, on_fail=on_fail))
+
+    return valid_formatters
+
+
+# def validate_element(element: ET.Element, strict: bool = False) -> Tuple[Dict, Prompt]:
+def validate_element(element: ET.Element, strict: bool = False) -> Dict:
+    """Validate an XML element.
+
+    Creates a schema with only the valid elements.
+
+    Args:
+        element: The XML element to validate.
+        strict: If True, raise an error if the element is not registered.
+
+    Returns:
+        A tuple of the prompt and the schema."""
+
+    schema = {}
+    shell_type = False
+
+    if 'name' not in element.attrib:
+
+        if element.tag == 'prompt':
+            # TODO(shreya): This needs to be more robust -- which tags are ok?
+            shell_type = True
+            pass
+        else:
+            raise ValueError(f"Element {element.tag} does not have a name.")
+
+    if not shell_type:
+        name = element.attrib['name']
+        if element.tag not in types_registry:
+            if strict:
+                raise ValueError(f"Element {element.tag} is not registered.")
+            else:
+                # Raise a warning tha the element is not registered.
+                warnings.warn(f"Element {element.tag} is not registered.")
+        else:
+            schema[name] = Field(datatype=types_registry[element.tag])
+            if 'format' in element.attrib:
+                schema[name].validators = get_formatters(element, strict=strict)
+
+    # If element has children, check that they are valid.
+
+    if shell_type:
+        for child in element:
+            schema.update(validate_element(child, strict=strict))
+    else:
+        for child in element:
+            schema[name].children = validate_element(child)
+
+    return schema
--- guardrails/x_validators.py
+++ guardrails/x_validators.py
@@ -1,10 +1,10 @@
 """Create validators for each data type."""
 from collections import defaultdict
-from typing import List, Union
+from typing import List, Union, Any, Optional, Callable
 
 from guardrails.x_datatypes import registry as types_registry
 
-validators_registry = defaultdict(list)
+validators_registry = {}
 types_to_validators = defaultdict(list)
 
 
@@ -14,12 +14,12 @@ def register_validator(name: str, data_type: Union[str, List[str]]):
     def decorator(cls: type):
         """Register a validator for a data type."""
 
+        nonlocal data_type
         if isinstance(data_type, str):
-
             if data_type == 'all':
                 data_type = list(types_registry.keys())
-
-            data_type = [data_type]
+            else:
+                data_type = [data_type]
 
         # Make sure that the data type string exists in the data types registry.
         for dt in data_type:
@@ -28,7 +28,7 @@ def register_validator(name: str, data_type: Union[str, List[str]]):
 
             types_to_validators[dt].append(name)
 
-        validators_registry[name].append(cls)
+        validators_registry[name] = cls
         return cls
 
     return decorator
@@ -36,6 +36,12 @@ def register_validator(name: str, data_type: Union[str, List[str]]):
 
 class Validator:
     """Base class for validators."""
+
+    def __init__(self, on_fail: Optional[Callable] = None):
+        if on_fail is not None:
+            self.on_fail = on_fail
+        else:
+            self.on_fail = self.debug
 
     def validate(self, value: Any) -> bool:
         """Validate a value."""
@@ -68,12 +74,13 @@ class Validator:
 #         return value is not None
 
 
-@register_validator('valid-range', ['integer', 'float', 'percentage'])
+@register_validator(name='valid-range', data_type=['integer', 'float', 'percentage'])
 class ValidRange(Validator):
     """Validate that a value is within a range."""
 
-    def __init__(self, min: int = None, max: int = None):
+    def __init__(self, min: int = None, max: int = None, on_fail: Optional[Callable] = None):
         """Initialize the validator."""
+        super().__init__(on_fail=on_fail)
 
         self._min = min
         self._max = max
@@ -94,13 +101,13 @@ class ValidRange(Validator):
         raise NotImplementedError
 
 
-@register_validator('valid-choices', 'all')
+@register_validator(name='valid-choices', data_type='all')
 class ValidChoices(Validator):
     """Validate that a value is within a range."""
 
-    def __init__(self, choices: List[Any]):
+    def __init__(self, choices: List[Any], on_fail: Optional[Callable] = None):
         """Initialize the validator."""
-
+        super().__init__(on_fail=on_fail)
         self._choices = choices
 
     def validate(self, value: Any) -> bool:
