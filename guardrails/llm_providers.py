d05a4526a06df17924c9e061fcb9ffb8728ae329
Karan Goel <kgoel93@gmail.com>
2023-03-14 23:30:19 -0700

    Generalize llm calls

--- guardrails/llm_providers.py
+++ guardrails/llm_providers.py
@@ -1,8 +1,39 @@
+from dataclasses import dataclass
 import os
 from functools import partial
 from typing import Callable
 
 import openai
+
+class PromptCallableException(Exception):
+    pass
+
+@dataclass
+class PromptCallable:
+    """
+    A wrapper around a callable that takes in a prompt.
+    Catches exceptions to let the user know clearly 
+    if the callable failed, and how to fix it.
+    """
+
+    fn: Callable
+
+    def __call__(self, *args, **kwargs):
+        try:
+            result = self.fn(*args, **kwargs)
+        except Exception as e:
+            raise PromptCallableException(
+                f"The callable `fn` passed to `Guard(fn, ...)` failed with the following error: {e}. "
+                "Make sure that `fn` can be called as a function that takes in a single prompt string "
+                "and returns a string."
+            )
+        if not isinstance(result, str):
+            raise PromptCallableException(
+                f"The callable `fn` passed to `Guard(fn, ...)` returned a non-string value: {result}. "
+                "Make sure that `fn` can be called as a function that takes in a single prompt string "
+                "and returns a string."
+            )
+        return result
 
 
 def openai_wrapper(text: str, *args, **kwargs):
@@ -15,9 +46,26 @@ def openai_wrapper(text: str, *args, **kwargs):
     )
     return openai_response["choices"][0]["text"]
 
+def openai_chat_wrapper(text: str, *args, model="gpt-3.5-turbo", **kwargs):
+    api_key = os.environ.get("OPENAI_API_KEY")
+    openai_response = openai.ChatCompletion.create(
+        api_key=api_key,
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": text},
+        ]
+        *args,
+        **kwargs,
+    )
+    return openai_response["choices"][0]['message']['content']
+
 
 def get_llm_ask(llm_api: Callable, *args, **kwargs):
     if llm_api == openai.Completion.create:
         return partial(openai_wrapper, *args, **kwargs)
+    elif llm_api == openai.ChatCompletion.create:
+        return partial(openai_chat_wrapper, *args, **kwargs)
     else:
-        raise ValueError("Only openai.Completion.create supported right now.")
+        # Let the user pass in an arbitrary callable.
+        return PromptCallable(fn=partial(llm_api, *args, **kwargs))
