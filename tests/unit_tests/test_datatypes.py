a45ca904db3ecb00d2ab4f78103cafca5c69d70d
Shreya Rajpal <shreya.rajpal@gmail.com>
2023-03-18 03:28:47 -0700

    lint

--- tests/unit_tests/test_datatypes.py
+++ tests/unit_tests/test_datatypes.py
@@ -11,7 +11,7 @@ from guardrails.datatypes import get_args
         ("{list(range(5))} a b", [[0, 1, 2, 3, 4], "a", "b"]),
         ("{[1, 2, 3]} a b", [[1, 2, 3], "a", "b"]),
         ("{{'a': 1, 'b': 2}} c d", [{"a": 1, "b": 2}, "c", "d"]),
-    ]
+    ],
 )
 def test_get_args(input_string, expected):
     assert get_args(input_string) == expected
