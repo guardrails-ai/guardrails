f4f1e13d0ddc2ffe3503fc13c6ecd6583f1c0bef
rafael <hello@irgolic.com>
2023-03-18 04:09:48 +0100

    Reask: Fix `repr` recursion error, remove use of `pretty_repr` (#54)
    * reask_utils: Fix ReAsk.__repr__ recursion error
    
    * reask_utils: Instead of pretty_repr, convert ReAsk to dict
    
    * reask_utils: Use json.dumps instead of rich.pretty.pretty_repr
    
    * test_reask_utils: Add prune_json_for_reasking and get_reask_prompt

--- tests/unit_tests/utils/test_reask_utils.py
+++ tests/unit_tests/utils/test_reask_utils.py
@@ -1,5 +1,9 @@
-import pytest
+import json
 
+import pytest
+from lxml import etree as ET
+
+from guardrails.utils import reask_utils
 from guardrails.utils.reask_utils import (
     ReAsk,
     gather_reasks,
@@ -53,3 +57,135 @@ def test_gather_reasks():
         ReAsk("h0", "Error Msg", "h1", ["h", 2, 2]),
     ]
     assert gather_reasks(input_dict) == expected_reasks
+
+
+@pytest.mark.parametrize(
+    "input_dict, expected_dict",
+    [
+        (
+            {"a": 1, "b": reask_utils.ReAsk(-1, "Error Msg", 1)},
+            {
+                "b": {
+                    "incorrect_value": -1,
+                    "error_message": "Error Msg",
+                    "fix_value": 1,
+                }
+            },
+        ),
+        (
+            {"a": 1, "b": {"c": 2, "d": reask_utils.ReAsk(-1, "Error Msg", 2)}},
+            {
+                "b": {
+                    "d": {
+                        "incorrect_value": -1,
+                        "error_message": "Error Msg",
+                        "fix_value": 2,
+                    }
+                }
+            },
+        ),
+        (
+            {"a": [1, 2, reask_utils.ReAsk(-1, "Error Msg", 3)], "b": 4},
+            {
+                "a": [
+                    {
+                        "incorrect_value": -1,
+                        "error_message": "Error Msg",
+                        "fix_value": 3,
+                    }
+                ]
+            },
+        ),
+        (
+            {"a": [1, 2, {"c": reask_utils.ReAsk(-1, "Error Msg", 3)}]},
+            {
+                "a": [
+                    {
+                        "c": {
+                            "incorrect_value": -1,
+                            "error_message": "Error Msg",
+                            "fix_value": 3,
+                        }
+                    }
+                ]
+            },
+        ),
+        ({"a": 1}, None),
+    ],
+)
+def test_prune_json_for_reasking(input_dict, expected_dict):
+    """Test that the prune_json_for_reasking function removes ReAsk objects."""
+    assert reask_utils.prune_json_for_reasking(input_dict) == expected_dict
+
+
+@pytest.mark.parametrize(
+    "example_rail, reasks, reask_json",
+    [
+        (
+            """
+<output>
+    <string name="name" required="true"/>
+</output>
+""",
+            [reask_utils.ReAsk(-1, "Error Msg", "name", ["name"])],
+            {
+                "name": {
+                    "incorrect_value": -1,
+                    "error_message": "Error Msg",
+                    "fix_value": "name",
+                }
+            },
+        ),
+        (
+            """
+<output>
+    <string name="name" required="true"/>
+    <integer name="age" required="true"/>
+</output>
+""",
+            [
+                reask_utils.ReAsk(-1, "Error Msg", "name", ["name"]),
+                reask_utils.ReAsk(-1, "Error Msg", "age", ["age"]),
+            ],
+            {
+                "name": {
+                    "incorrect_value": -1,
+                    "error_message": "Error Msg",
+                    "fix_value": "name",
+                },
+                "age": {
+                    "incorrect_value": -1,
+                    "error_message": "Error Msg",
+                    "fix_value": "age",
+                },
+            },
+        ),
+    ],
+)
+def test_get_reask_prompt(example_rail, reasks, reask_json):
+    """Test that get_reask_prompt function returns the correct prompt."""
+    expected_result_template = """
+I was given the following JSON response, which had problems due to incorrect values.
+
+%s
+
+Help me correct the incorrect values based on the given error messages.
+
+Given below is XML that describes the information to extract from this document and the tags to extract it into.
+%s
+ONLY return a valid JSON object (no other text is necessary), where the key of the field in JSON is the `name` attribute of the corresponding XML, and the value is of the type specified by the corresponding XML's tag. The JSON MUST conform to the XML format, including any types and format requests e.g. requests for lists, objects and specific types. Be correct and concise. If you are unsure anywhere, enter `None`.
+
+Here are examples of simple (XML, JSON) pairs that show the expected behavior:
+- `<string name='foo' format='two-words lower-case' />` => `{{'foo': 'example one'}}`
+- `<list name='bar'><string format='upper-case' /></list>` => `{{"bar": ['STRING ONE', 'STRING TWO', etc.]}}`
+- `<object name='baz'><string name="foo" format="capitalize two-words" /><integer name="index" format="1-indexed" /></object>` => `{{'baz': {{'foo': 'Some String', 'index': 1}}}}`
+
+JSON Object:"""  # noqa: E501
+
+    result_prompt, _ = reask_utils.get_reask_prompt(
+        ET.fromstring(example_rail), reasks, reask_json
+    )
+    assert result_prompt == expected_result_template % (
+        json.dumps(reask_json, indent=2),
+        example_rail,
+    )
