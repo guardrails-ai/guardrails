4f3c15a20b292cba8db2dc2044fc883c8ed75865
Shreya Rajpal <shreya.rajpal@gmail.com>
2023-03-17 23:09:59 -0700

    added tests

--- tests/integration_tests/test_guard.py
+++ tests/integration_tests/test_guard.py
@@ -1,6 +1,8 @@
 import pytest
 
-from guardrails import Guard
+import guardrails as gd
+
+from .mock_llm_outputs import entity_extraction, openai_Completion_create
 
 
 @pytest.fixture(scope="module")
@@ -74,5 +76,46 @@ def validated_output():
 def test_rail_spec_output_parse(rail_spec, llm_output, validated_output):
     """Test that the rail_spec fixture is working."""
 
-    guard = Guard.from_rail_string(rail_spec)
+    guard = gd.Guard.from_rail_string(rail_spec)
     assert guard.parse(llm_output) == validated_output
+
+
+def test_entity_extraction_with_reask(mocker):
+    """Test that the entity extraction works with re-asking."""
+    mocker.patch(
+        "guardrails.llm_providers.openai_wrapper", new=openai_Completion_create
+    )
+
+    import openai
+
+    content = gd.docs_utils.read_pdf("docs/examples/data/chase_card_agreement.pdf")
+    guard = gd.Guard.from_rail_string(entity_extraction.RAIL_SPEC_WITH_REASK)
+    _, final_output = guard(
+        llm_api=openai.Completion.create,
+        prompt_params={"document": content[:6000]},
+        num_reasks=1,
+    )
+
+    # Assertions are made on the guard state object.
+    assert final_output == entity_extraction.VALIDATED_OUTPUT_REASK_2
+
+    guard_history = guard.guard_state.most_recent_call.history
+
+    # Check that the guard state object has the correct number of re-asks.
+    assert len(guard_history) == 2
+
+    # For orginal prompt and output
+    assert guard_history[0].prompt == entity_extraction.COMPILED_PROMPT_REASK_1
+    assert guard_history[0].output == entity_extraction.LLM_OUTPUT_REASK_1
+    assert (
+        guard_history[0].validated_response
+        == entity_extraction.VALIDATED_OUTPUT_REASK_1
+    )
+
+    # For re-asked prompt and output
+    assert guard_history[1].prompt == entity_extraction.COMPILED_PROMPT_REASK_2
+    assert guard_history[1].output == entity_extraction.LLM_OUTPUT_REASK_2
+    assert (
+        guard_history[1].validated_response
+        == entity_extraction.VALIDATED_OUTPUT_REASK_2
+    )
