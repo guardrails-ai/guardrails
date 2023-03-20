e44c93cbb918ff18caebb4c928dc89eb53a849e7
Shreya Rajpal <shreya.rajpal@gmail.com>
2023-03-19 18:34:58 -0700

    Move PromptCallable to the top level imports

--- guardrails/__init__.py
+++ guardrails/__init__.py
@@ -1,12 +1,14 @@
 # Set up __init__.py so that users can do from guardrails import Response, Schema, etc.
 
 from guardrails.guard import Guard
+from guardrails.llm_providers import PromptCallable
 from guardrails.rail import Rail
 from guardrails.utils import constants, docs_utils
 from guardrails.validators import Validator, register_validator
 
 __all__ = [
     "Guard",
+    "PromptCallable",
     "Rail",
     "Validator",
     "register_validator",
