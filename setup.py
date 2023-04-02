61325cdcc26c36c5b220d1462d71256477d7ca6c
Shreya Rajpal <shreya.rajpal@gmail.com>
2023-04-01 20:40:32 -0700

    support guardrails cli

--- setup.py
+++ setup.py
@@ -131,6 +131,10 @@ setup(
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
+    # Add CLI for "guardrails" command, point to guardrails.cli:cli
+    entry_points={
+        "console_scripts": ["guardrails=guardrails.cli:cli"],
+    },
     install_requires=REQUIRED,
     include_package_data=True,
     extras_require=EXTRAS,
