32f1af53ddfdf743b40f0e87d7ec64676aefc16e
Shreya Rajpal <shreya.rajpal@gmail.com>
2023-04-15 16:06:07 -0700

    updated eliot logging

--- guardrails/guard.py
+++ guardrails/guard.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Callable, Dict, Optional, Tuple
 
-from eliot import start_action, to_file
+from eliot import add_destinations, start_action
 
 from guardrails.llm_providers import PromptCallable, get_llm_ask
 from guardrails.prompt import Prompt
@@ -12,7 +12,8 @@ from guardrails.utils.logs_utils import GuardState
 from guardrails.utils.reask_utils import sub_reasks_with_fixed_values
 
 logger = logging.getLogger(__name__)
-# to_file(open("guardrails.log", "w"))
+actions_logger = logging.getLogger(f"{__name__}.actions")
+add_destinations(actions_logger.debug)
 
 
 class Guard:
--- guardrails/logging.py
+++ guardrails/logging.py
@@ -1,40 +1,10 @@
 import logging
 import logging.config
 
-LOGGING_CONFIG = {
-    "version": 1,
-    "formatters": {
-        "default": {
-            "format": "[%(asctime)s] %(levelname)s %(name)s: %(message)s",
-        },
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "level": "DEBUG",
-            "formatter": "default",
-            "stream": "ext://sys.stdout",
-        },
-    },
-    "loggers": {
-        "your_package": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-    },
-    "root": {
-        "level": "WARNING",
-        "handlers": ["console"],
-    },
-}
-
 
 def configure_logging(logging_config=None, log_level=None):
-    if logging_config is None:
-        logging_config = LOGGING_CONFIG
-
-    logging.config.dictConfig(logging_config)
+    if logging_config is not None:
+        logging.config.dictConfig(logging_config)
 
     if log_level is not None:
         logger = logging.getLogger(__name__)
