4c3220c10e77480fa421c9bbdcea6337193523a7
Shreya Rajpal <shreya.rajpal@gmail.com>
2023-04-21 00:37:46 -0700

    add integration test

---
+++ tests/integration_tests/applications/test_text2sql.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+import pytest
+
+from guardrails.applications.text2sql import Text2Sql
+
+CURRENT_DIR_PARENT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SCHEMA_PATH = os.path.join(CURRENT_DIR_PARENT, "test_assets/text2sql/schema.sql")
+EXAMPLES_PATH = os.path.join(CURRENT_DIR_PARENT, "test_assets/text2sql/examples.json")
+DB_PATH = os.path.join(
+    CURRENT_DIR_PARENT, "test_assets/text2sql/department_management.sqlite"
+)
+
+
+@pytest.mark.parametrize(
+    "conn_str, schema_path, examples",
+    [
+        ("sqlite://", SCHEMA_PATH, EXAMPLES_PATH),
+        (f"sqlite:///{DB_PATH}", None, None),
+    ],
+)
+def test_text2sql_with_examples(conn_str: str, schema_path: str, examples: str, mocker):
+    """Test that Text2Sql can be initialized with examples."""
+
+    # Mock the call to the OpenAI API.
+    mocker.patch(
+        "guardrails.embedding.OpenAIEmbedding._get_embedding",
+        new=lambda *args, **kwargs: [0.1] * 1536,
+    )
+
+    if examples is not None:
+        with open(examples, "r") as f:
+            examples = json.load(f)
+
+    # This should not raise an exception.
+    Text2Sql(conn_str, schema_file=schema_path, examples=examples)
