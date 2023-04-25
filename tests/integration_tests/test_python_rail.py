316d44ed192fad4201a870b7f1d54b5560248e87
Willem Pienaar <github@willem.co>
2023-04-25 08:56:57 -0700

    feat(rail.py): add support for generating XML code for details of movies feat(rail.py): add support for name of director in output test(test_python_rail.py): update test case to include name of director and details of movies

--- tests/integration_tests/test_python_rail.py
+++ tests/integration_tests/test_python_rail.py
@@ -1,9 +1,10 @@
 import json
+from datetime import date, time
+from typing import List
 
 import openai
+from pydantic import BaseModel, EmailStr
 
-from pydantic import BaseModel
-from typing import List
 import guardrails as gd
 
 from .mock_llm_outputs import openai_completion_create
@@ -11,21 +12,33 @@ from .test_assets import python_rail
 
 
 def test_python_rail_with_reask(mocker):
-
     mocker.patch(
         "guardrails.llm_providers.openai_wrapper", new=openai_completion_create
     )
 
+    class Details(BaseModel):
+        release_date: date
+        duration: time
+        budget: float
+        is_sequel: bool
+        website: str
+        contact_email: EmailStr
+
     class Movie(BaseModel):
         rank: int
         title: str
+        details: Details
 
-    class Movies(BaseModel):
+    class Director(BaseModel):
+        name: str
         movies: List[Movie]
 
     guard = gd.Guard.from_class(
-        output_class=Movies,
-        prompt="What are the top 5 grossing movies from {{director}}?"
+        output_class=Director,
+        prompt=(
+            "Provide detailed information about the top 5 grossing movies from {{director}} "
+            "including release date, duration, budget, whether it's a sequel, website, and contact email."
+        ),
     )
 
     _, final_output = guard(
@@ -40,4 +53,3 @@ def test_python_rail_with_reask(mocker):
     # Assertions are made on the guard state object.
     expected_llm_output_dict = json.loads(python_rail.LLM_OUTPUT)
     assert final_output == expected_llm_output_dict
-
