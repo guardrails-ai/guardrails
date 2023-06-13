caae288e4d14ed1ba8a0ca9a63aace92cb456da5
Rafael Irgolic <hello@irgolic.com>
2023-06-13 11:42:11 +0100

    Fixup skeleton util method, add test

--- guardrails/utils/json_utils.py
+++ guardrails/utils/json_utils.py
@@ -1,25 +1,27 @@
 from dataclasses import dataclass
-from lxml import etree as ET
+import lxml.etree as ET
 from typing import Any, Dict
 
 
 @dataclass
 class Placeholder:
-    expected_type: str
+    type_map = {
+        "string": str,
+        "integer": int,
+        "float": float,
+        "bool": bool,
+        "object": dict,
+        "list": list,
+    }
 
-    @classmethod
-    def type_dict(cls):
-        return {
-            "string": str,
-            "integer": int,
-            "float": float,
-            "bool": bool,
-            "object": dict,
-            "list": list,
-        }
+    type_string: str
+
+    @property
+    def type_object(self):
+        return self.type_map[self.type_string]
 
 
-def generate_json_skeleton_from_schema(schema: ET._Element) -> Dict[str, Any]:
+def generate_type_skeleton_from_schema(schema: ET._Element) -> Dict[str, Any]:
     """Generate a JSON skeleton from an XML schema."""
 
     def _recurse_schema(schema):
@@ -41,7 +43,7 @@ def generate_json_skeleton_from_schema(schema: ET._Element) -> Dict[str, Any]:
 def verify_schema_against_json(xml_schema: ET._Element, generated_json: Dict[str, Any]):
     """Verify that a JSON schema is valid for a given XML."""
 
-    json_schema = generate_json_skeleton_from_schema(xml_schema)
+    type_skeleton = generate_type_skeleton_from_schema(xml_schema)
 
     def _verify_dict(schema, json):
         if set(schema.keys()) != set(json.keys()):
@@ -49,22 +51,21 @@ def verify_schema_against_json(xml_schema: ET._Element, generated_json: Dict[str
 
         for key in schema.keys():
             if isinstance(schema[key], Placeholder):
-                expected_type = Placeholder.type_dict[schema[key].expected_type]
+                expected_type = schema[key].type_object
                 if not isinstance(json[key], expected_type):
                     return False
+            elif isinstance(schema[key], dict):
+                if not isinstance(json[key], dict):
+                    return False
+                if not _verify_dict(schema[key], json[key]):
+                    return False
+            elif isinstance(schema[key], list):
+                if not isinstance(json[key], list):
+                    return False
+                if not _verify_list(schema[key], json[key]):
+                    return False
             else:
-                if isinstance(schema[key], dict):
-                    if not isinstance(json[key], dict):
-                        return False
-                    if not _verify_dict(schema[key], json[key]):
-                        return False
-                elif isinstance(schema[key], list):
-                    if not isinstance(json[key], list):
-                        return False
-                    if not _verify_list(schema[key][0], json[key][0]):
-                        return False
-                else:
-                    raise ValueError(f"Unknown type {type(schema[key])}")
+                raise ValueError(f"Unknown type {type(schema[key])}")
 
         return True
     
@@ -72,27 +73,30 @@ def verify_schema_against_json(xml_schema: ET._Element, generated_json: Dict[str
         assert len(schema) == 1  # Schema for a list should only have one child
         if not isinstance(json, list):
             return False
+
+        child_schema = schema[0]
         
-        if isinstance(schema[0], Placeholder):
-            expected_type = Placeholder.type_dict[schema[0].expected_type]
+        if isinstance(child_schema, Placeholder):
+            expected_type = child_schema.type_object
 
             for item in json:
                 if not isinstance(item, expected_type):
                     return False
-        else:
-            expected_type = type(schema[0])
+        elif isinstance(child_schema, dict):
             for item in json:
-                if not isinstance(item, expected_type):
+                if not isinstance(item, dict):
                     return False
-                if isinstance(item, dict):
-                    if not _verify_dict(schema[0], item):
-                        return False
-                elif isinstance(item, list):
-                    if not _verify_list(schema[0], item):
-                        return False
-                else:
-                    raise ValueError(f"Unknown type {type(item)}")
+                if not _verify_dict(child_schema, item):
+                    return False
+        elif isinstance(child_schema, list):
+            for item in json:
+                if not isinstance(item, list):
+                    return False
+                if not _verify_list(child_schema, item):
+                    return False
+        else:
+            raise ValueError(f"Unknown type {type(child_schema)}")
         
         return True
 
-    return _verify_dict(json_schema, generated_json)
+    return _verify_dict(type_skeleton, generated_json)
