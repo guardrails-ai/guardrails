22f670014f0e314a719c80fa0c9cb2083e5e3be9
Rafael Irgolic <hello@irgolic.com>
2023-06-21 16:34:45 +0100

    json_utils: Refactor into placeholder methods

--- guardrails/utils/json_utils.py
+++ guardrails/utils/json_utils.py
@@ -1,11 +1,24 @@
 from dataclasses import dataclass
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import lxml.etree as ET
 
 
 @dataclass
 class Placeholder:
+    optional: bool
+
+    def verify(
+        self,
+        json_value,
+        prune_extra_keys: bool = True,
+        coerce_types: bool = False,
+    ) -> bool:
+        raise NotImplementedError
+
+
+@dataclass
+class ValuePlaceholder(Placeholder):
     type_map = {
         "string": str,
         "integer": int,
@@ -30,36 +43,212 @@ class Placeholder:
             return Any
         return self.type_map[self.type_string]
 
+    @staticmethod
+    def verification_failed():
+        # Sentinel value
+        pass
+
+    def verify(
+        self,
+        json_value,
+        prune_extra_keys: bool = False,
+        coerce_types=False
+    ):
+        expected_type = self.type_object
+        if expected_type == Any:
+            return json_value
+        if not isinstance(json_value, expected_type):
+            if not coerce_types:
+                return self.verification_failed
+            try:
+                return expected_type(json_value)
+            except ValueError:
+                return self.verification_failed
+        return json_value
+
 
 @dataclass
-class Choice:
+class DictPlaceholder(Placeholder):
+    children: Dict[str, Any]
+
+    def verify(
+        self,
+        json_value,
+        prune_extra_keys: bool = False,
+        coerce_types: bool = False,
+    ) -> bool:
+        if not isinstance(json_value, dict):
+            return False
+        if not self.children.keys():
+            return True
+        json_keys = set(json_value.keys())
+
+        schema_keys = set(self.children.keys())
+        choice_keys = set()
+        for key in schema_keys:
+            if isinstance(self.children[key], ChoicePlaceholder):
+                choice_keys.update(self.children[key].cases.keys())
+
+        extra_keys = json_keys - schema_keys - choice_keys
+        if prune_extra_keys and extra_keys:
+            for key in extra_keys:
+                del json_value[key]
+        if any(key not in json_keys for key in schema_keys):
+            return False
+
+        for key, placeholder in self.children.items():
+            if isinstance(placeholder, ValuePlaceholder):
+                value = placeholder.verify(
+                    json_value[key],
+                    prune_extra_keys=prune_extra_keys,
+                    coerce_types=coerce_types,
+                )
+                if value is ValuePlaceholder.verification_failed:
+                    return False
+                json_value[key] = value
+            elif isinstance(placeholder, ChoicePlaceholder):
+                if not placeholder.verify(
+                    json_value,
+                    prune_extra_keys=prune_extra_keys,
+                    coerce_types=coerce_types,
+                ):
+                    return False
+            else:
+                if not placeholder.verify(
+                    json_value[key],
+                    prune_extra_keys=prune_extra_keys,
+                    coerce_types=coerce_types,
+                ):
+                    return False
+
+        return True
+
+
+@dataclass
+class ListPlaceholder(Placeholder):
+    child: Optional[Placeholder]
+
+    def verify(
+        self,
+        json_value,
+        prune_extra_keys: bool = True,
+        coerce_types: bool = False,
+    ) -> bool:
+        if not isinstance(json_value, list):
+            return False
+
+        if isinstance(self.child, ValuePlaceholder):
+            for i, item in enumerate(json_value):
+                value = self.child.verify(
+                    item,
+                    prune_extra_keys=prune_extra_keys,
+                    coerce_types=coerce_types,
+                )
+                if value is ValuePlaceholder.verification_failed:
+                    return False
+                json_value[i] = value
+            return True
+        else:
+            for item in json_value:
+                if not self.child.verify(item):
+                    return False
+
+        return True
+
+
+@dataclass
+class ChoicePlaceholder(Placeholder):
     name: str
     cases: Dict[str, Any]
 
+    def verify(
+        self,
+        json_value,
+        prune_extra_keys: bool = True,
+        coerce_types: bool = False,
+    ) -> bool:
+        if not isinstance(json_value, dict):
+            return False
+        if self.name not in json_value:
+            return False
 
-def generate_type_skeleton_from_schema(schema: ET._Element) -> Dict[str, Any]:
+        value_name = json_value[self.name]
+        if value_name not in self.cases:
+            return False
+        if value_name not in json_value:
+            return False
+        if any(key in json_value for key in self.cases.keys() if key != value_name):
+            return False
+
+        value_schema = self.cases[value_name]
+        value = json_value[value_name]
+        if isinstance(value_schema, ValuePlaceholder):
+            value = value_schema.verify(
+                value,
+                prune_extra_keys=prune_extra_keys,
+                coerce_types=coerce_types,
+            )
+            if value is ValuePlaceholder.verification_failed:
+                return False
+            json_value[value_name] = value
+        else:
+            if not value_schema.verify(
+                value,
+                prune_extra_keys=prune_extra_keys,
+                coerce_types=coerce_types,
+            ):
+                return False
+
+        return True
+
+
+def generate_type_skeleton_from_schema(schema: ET._Element) -> DictPlaceholder:
     """Generate a JSON skeleton from an XML schema."""
 
     def _recurse_schema(schema):
+        is_optional = schema.attrib.get("required", "true") == "false"
         if schema.tag == "object":
-            return {child.attrib["name"]: _recurse_schema(child) for child in schema}
+            return DictPlaceholder(
+                children={
+                    child.attrib["name"]:
+                    _recurse_schema(child) for child in schema
+                },
+                optional=is_optional,
+            )
         elif schema.tag == "list":
             if len(schema) == 0:
-                return []
-            return [_recurse_schema(schema[0])]
+                child = None
+            elif len(schema) == 1:
+                child = _recurse_schema(schema[0])
+            else:
+                raise ValueError("List must have exactly zero or one child")
+            return ListPlaceholder(
+                child=child,
+                optional=is_optional,
+            )
         elif schema.tag == "choice":
-            return Choice(
-                schema.attrib["name"],
-                {
+            return ChoicePlaceholder(
+                name=schema.attrib["name"],
+                cases={
                     child.attrib["name"]: _recurse_schema(child[0])
                     for child in schema
                     if child.tag == "case"
                 },
+                optional=is_optional,
             )
         else:
-            return Placeholder(schema.tag)
+            return ValuePlaceholder(
+                type_string=schema.tag,
+                optional=is_optional,
+            )
 
-    return {child.attrib["name"]: _recurse_schema(child) for child in schema}
+    return DictPlaceholder(
+        children={
+            child.attrib["name"]:
+            _recurse_schema(child) for child in schema
+        },
+        optional=False,
+    )
 
 
 def verify_schema_against_json(
@@ -71,129 +260,8 @@ def verify_schema_against_json(
     """Verify that a JSON schema is valid for a given XML."""
 
     type_skeleton = generate_type_skeleton_from_schema(xml_schema)
-
-    def verification_failed():
-        # Sentinel value
-        pass
-
-    def _verify_and_cast_value(placeholder, value):
-        expected_type = placeholder.type_object
-        if expected_type == Any:
-            return value
-        if not isinstance(value, expected_type):
-            if not coerce_types:
-                return verification_failed
-            try:
-                return expected_type(value)
-            except ValueError:
-                return verification_failed
-        return value
-
-    def _verify_dict(schema, json):
-        if not isinstance(json, dict):
-            return False
-        if not schema.keys():
-            return True
-        json_keys = set(json.keys())
-
-        schema_keys = set(schema.keys())
-        choice_keys = set()
-        for key in schema_keys:
-            if isinstance(schema[key], Choice):
-                choice_keys.update(schema[key].cases.keys())
-
-        extra_keys = json_keys - schema_keys - choice_keys
-        if prune_extra_keys and extra_keys:
-            for key in extra_keys:
-                del json[key]
-        if any(key not in json_keys for key in schema_keys):
-            return False
-
-        for key in schema.keys():
-            if isinstance(schema[key], Placeholder):
-                value = _verify_and_cast_value(schema[key], json[key])
-                if value is verification_failed:
-                    return False
-                json[key] = value
-            elif isinstance(schema[key], dict):
-                if not _verify_dict(schema[key], json[key]):
-                    return False
-            elif isinstance(schema[key], list):
-                if not _verify_list(schema[key], json[key]):
-                    return False
-            elif isinstance(schema[key], Choice):
-                if not _verify_choice(schema[key], json):
-                    return False
-            else:
-                raise ValueError(f"Unknown type {type(schema[key])}")
-
-        return True
-
-    def _verify_list(schema, json):
-        if not isinstance(json, list):
-            return False
-        if len(schema) == 0:
-            return True
-        if len(schema) > 1:
-            raise ValueError("List schema should only have one child")
-        if not isinstance(json, list):
-            return False
-
-        child_schema = schema[0]
-
-        if isinstance(child_schema, Placeholder):
-            for i, item in enumerate(json):
-                value = _verify_and_cast_value(child_schema, item)
-                if value is verification_failed:
-                    return False
-                json[i] = value
-            return True
-        elif isinstance(child_schema, dict):
-            verification_func = _verify_dict
-        elif isinstance(child_schema, list):
-            verification_func = _verify_list
-        elif isinstance(child_schema, Choice):
-            verification_func = _verify_choice
-        else:
-            raise ValueError(f"Unknown type {type(child_schema)}")
-
-        for item in json:
-            if not verification_func(child_schema, item):
-                return False
-
-        return True
-
-    def _verify_choice(schema, json):
-        if not isinstance(json, dict):
-            return False
-        if schema.name not in json:
-            return False
-        value_name = json[schema.name]
-        if value_name not in schema.cases:
-            return False
-        if value_name not in json:
-            return False
-        if any(key in json for key in schema.cases.keys() if key != value_name):
-            return False
-        value_schema = schema.cases[value_name]
-        value = json[value_name]
-        if isinstance(value_schema, Placeholder):
-            value = _verify_and_cast_value(value_schema, value)
-            if value is verification_failed:
-                return False
-            json[value_name] = value
-        elif isinstance(value_schema, dict):
-            if not _verify_dict(value_schema, value):
-                return False
-        elif isinstance(value_schema, list):
-            if not _verify_list(value_schema, value):
-                return False
-        elif isinstance(value_schema, Choice):
-            if not _verify_choice(value_schema, value):
-                return False
-        else:
-            raise ValueError(f"Unknown type {type(value_schema)}")
-
-        return True
-
-    return _verify_dict(type_skeleton, generated_json)
+    return type_skeleton.verify(
+        generated_json,
+        prune_extra_keys=prune_extra_keys,
+        coerce_types=coerce_types,
+    )
