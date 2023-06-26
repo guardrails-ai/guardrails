85785af5b91cee43e80c42a1c33ce295c7340509
Rafael Irgolic <hello@irgolic.com>
2023-06-26 13:00:06 +0100

    validators: Add back ValidUrl

--- guardrails/validators.py
+++ guardrails/validators.py
@@ -729,6 +729,44 @@ class OneLine(Validator):
         return schema
 
 
+@register_validator(name="valid-url", data_type=["string", "url"])
+class ValidURL(Validator):
+    """Validate that a value is a valid URL.
+
+    - Name for `format` attribute: `valid-url`
+    - Supported data types: `string`, `url`
+    - Programmatic fix: None
+    """
+
+    def validate(self, key: str, value: Any, schema: Union[Dict, List]) -> Dict:
+        logger.debug(f"Validating {value} is a valid URL...")
+
+        from urllib.parse import urlparse
+
+        # Check that the URL is valid
+        try:
+            result = urlparse(value)
+            # Check that the URL has a scheme and network location
+            if not result.scheme or not result.netloc:
+                raise EventDetail(
+                    key,
+                    value,
+                    schema,
+                    f"URL {value} is not valid.",
+                    None,
+                )
+        except ValueError:
+            raise EventDetail(
+                key,
+                value,
+                schema,
+                f"URL {value} is not valid.",
+                None,
+            )
+
+        return schema
+
+
 @register_validator(name="is-reachable", data_type=["string", "url"])
 class EndpointIsReachable(Validator):
     """Validate that a value is a reachable URL.
