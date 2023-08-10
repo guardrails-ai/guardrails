8797fab1331f0095cb76f6ccd9ef08d0d3e7a6fc
Rafael Irgolic <hello@irgolic.com>
2023-08-10 15:37:14 +0200

    validators: Allow functions as validators

--- guardrails/validators.py
+++ guardrails/validators.py
@@ -165,22 +165,38 @@ def register_validator(name: str, data_type: Union[str, List[str]]):
     """Register a validator for a data type."""
     from guardrails.datatypes import registry as types_registry
 
-    def decorator(cls: type):
+    if isinstance(data_type, str):
+        data_type = list(types_registry.keys()) if data_type == "all" else [data_type]
+    # Make sure that the data type string exists in the data types registry.
+    for dt in data_type:
+        if dt not in types_registry:
+            raise ValueError(f"Data type {dt} is not registered.")
+
+        types_to_validators[dt].append(name)
+
+    def decorator(cls_or_func: Union[type, Callable]):
         """Register a validator for a data type."""
-        nonlocal data_type
-        if isinstance(data_type, str):
-            data_type = (
-                list(types_registry.keys()) if data_type == "all" else [data_type]
+        if isinstance(cls_or_func, type):
+            cls = cls_or_func
+            cls.rail_alias = name
+        elif callable(cls_or_func):
+            func = cls_or_func
+            # ensure function takes two args
+            if not func.__code__.co_argcount == 2:
+                raise ValueError(
+                    f"Validator function {func.__name__} must take two arguments."
+                )
+            # dynamically create Validator subclass with `validate` method as `func`
+            cls = type(
+                name,
+                (Validator,),
+                {"validate": staticmethod(func), "rail_alias": name},
             )
-        # Make sure that the data type string exists in the data types registry.
-        for dt in data_type:
-            if dt not in types_registry:
-                raise ValueError(f"Data type {dt} is not registered.")
-
-            types_to_validators[dt].append(name)
-
+        else:
+            raise ValueError(
+                "Only classes and functions can be registered as validators."
+            )
         validators_registry[name] = cls
-        cls.rail_alias = name
         return cls
 
     return decorator
