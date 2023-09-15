f7cee6b8dcf9f9840bfe7971280b62750ae57638
Nefertiti  Rogers <nefertitirogers@Nefertitis-MacBook-Pro.local>
2023-09-15 15:37:44 -0700

    lint

--- tests/unit_tests/test_validator_suite.py
+++ tests/unit_tests/test_validator_suite.py
@@ -1,62 +1,87 @@
-import os
-import pytest
-import openai
-from pydantic import BaseModel, Field
-from guardrails.guard import Guard
-from typing import Dict
 import importlib
+import os
+from typing import Dict
+
+import openai
+import pytest
 from integration_tests.mock_llm_outputs import MockOpenAICallable
-from .validators.test_parameters import validator_test_pass_fail, validator_test_python_str, validator_test_xml
+from pydantic import BaseModel, Field
+
+from guardrails.guard import Guard
 from guardrails.validators import FailResult
 
-@pytest.mark.parametrize('validator_test_data', [(validator_test_pass_fail)])
+from .validators.test_parameters import (
+    validator_test_pass_fail,
+    validator_test_python_str,
+    validator_test_xml,
+)
+
+
+@pytest.mark.parametrize("validator_test_data", [(validator_test_pass_fail)])
 def test_validator_validate(validator_test_data: Dict[str, Dict[str, str]]):
     for validator_name in validator_test_data:
-        print('testing validator: ', validator_name)
+        print("testing validator: ", validator_name)
         module = importlib.import_module("guardrails.validators")
         validator_class = getattr(module, validator_name)
-        if 'instance_variables' in validator_test_data[validator_name]: 
-            instance = validator_class(**validator_test_data[validator_name]['instance_variables'])
+        if "instance_variables" in validator_test_data[validator_name]:
+            instance = validator_class(
+                **validator_test_data[validator_name]["instance_variables"]
+            )
         else:
             instance = validator_class()
-        result = instance.validate(validator_test_data[validator_name]['input_data'], validator_test_data[validator_name]['metadata'])
-        assert isinstance(result, validator_test_data[validator_name]['expected_result'])
-        
-        if isinstance(result, FailResult) and 'fix_value' in validator_test_data[validator_name]: 
-            assert result.fix_value == validator_test_data[validator_name]['fix_value']
+        result = instance.validate(
+            validator_test_data[validator_name]["input_data"],
+            validator_test_data[validator_name]["metadata"],
+        )
+        assert isinstance(
+            result, validator_test_data[validator_name]["expected_result"]
+        )
 
-@pytest.mark.parametrize('validator_test_data', [(validator_test_python_str)])
-def test_validator_python_string(mocker, validator_test_data: Dict[str, Dict[str, str]]): 
+        if (
+            isinstance(result, FailResult)
+            and "fix_value" in validator_test_data[validator_name]
+        ):
+            assert result.fix_value == validator_test_data[validator_name]["fix_value"]
+
+
+@pytest.mark.parametrize("validator_test_data", [(validator_test_python_str)])
+def test_validator_python_string(
+    mocker, validator_test_data: Dict[str, Dict[str, str]]
+):
     mocker.patch("guardrails.llm_providers.OpenAICallable", new=MockOpenAICallable)
 
     for validator_name in validator_test_data:
-        print('testing validator: ', validator_name)
+        print("testing validator: ", validator_name)
         module = importlib.import_module("guardrails.validators")
         validator_class = getattr(module, validator_name)
         validators = [validator_class(on_fail="reask")]
         guard = Guard.from_string(
-        validators, description=validator_test_data[validator_name]['description'], prompt=validator_test_data[validator_name]['prompt'], instructions=validator_test_data[validator_name]['instructions'],
+            validators,
+            description=validator_test_data[validator_name]["description"],
+            prompt=validator_test_data[validator_name]["prompt"],
+            instructions=validator_test_data[validator_name]["instructions"],
         )
         _, final_output = guard(
-        llm_api=openai.Completion.create,
-        prompt_params=validator_test_data[validator_name]['prompt_params'],
-        num_reasks=1,
-        max_tokens=100,
+            llm_api=openai.Completion.create,
+            prompt_params=validator_test_data[validator_name]["prompt_params"],
+            num_reasks=1,
+            max_tokens=100,
         )
 
-        assert final_output == validator_test_data[validator_name]['output']
+        assert final_output == validator_test_data[validator_name]["output"]
 
 
-@pytest.mark.parametrize('validator_test_data', [(validator_test_xml)])
+@pytest.mark.parametrize("validator_test_data", [(validator_test_xml)])
 def test_validator_to_xml(validator_test_data: Dict[str, Dict[str, str]]):
-        for validator_name in validator_test_data:
-            module = importlib.import_module("guardrails.validators")
-            print('testing validator: ', validator_name)
-            validator_class = getattr(module, validator_name)
-            if 'instance_variables' in validator_test_data[validator_name]: 
-                instance = validator_class(**validator_test_data[validator_name]['instance_variables'])
-            else:
-                instance = validator_class()
-            xml = instance.to_xml_attrib()
-            assert xml == validator_test_data[validator_name]['expected_xml']
-        
+    for validator_name in validator_test_data:
+        module = importlib.import_module("guardrails.validators")
+        print("testing validator: ", validator_name)
+        validator_class = getattr(module, validator_name)
+        if "instance_variables" in validator_test_data[validator_name]:
+            instance = validator_class(
+                **validator_test_data[validator_name]["instance_variables"]
+            )
+        else:
+            instance = validator_class()
+        xml = instance.to_xml_attrib()
+        assert xml == validator_test_data[validator_name]["expected_xml"]
--- tests/unit_tests/validators/test_parameters.py
+++ tests/unit_tests/validators/test_parameters.py
@@ -1,239 +1,230 @@
 from guardrails.validators import FailResult, PassResult
 
-validator_test_pass_fail = { 
-    "BugFreeSQL": { 
-        'input_data': 'select name, fro employees',
-        'metadata': {}, 
-        'expected_result': FailResult
-    }, 
-    "BugFreeSQL": { 
-        'input_data': 'select name from employees;',
-        'metadata': {}, 
-        'expected_result': PassResult
-    }, 
-    "ExtractedSummarySentencesMatch": { 
-        'input_data': 'It was a nice day. I went to the park. I saw a dog.', 
-        'metadata': {
-        "filepaths": [
-            "./tests/unit_tests/test_assets/article1.txt",
-            "./tests/unit_tests/test_assets/article2.txt",
-        ]
+validator_test_pass_fail = {
+    "BugFreeSQL": {
+        "input_data": "select name, fro employees",
+        "metadata": {},
+        "expected_result": FailResult,
+    },
+    "BugFreeSQL": {
+        "input_data": "select name from employees;",
+        "metadata": {},
+        "expected_result": PassResult,
+    },
+    "ExtractedSummarySentencesMatch": {
+        "input_data": "It was a nice day. I went to the park. I saw a dog.",
+        "metadata": {
+            "filepaths": [
+                "./tests/unit_tests/test_assets/article1.txt",
+                "./tests/unit_tests/test_assets/article2.txt",
+            ]
         },
-        'expected_result': PassResult
-    }, 
-    "ValidLength": {
-        "input_data": 'hello there yo', 
-        "metadata": {},
-        "expected_result": PassResult, 
-        "instance_variables": { 
-            'min': 4, 
-            'max': 17
-        }
-    }, 
-    "ValidLength": {
-        "input_data": 'hello there, this is getting wordyyy', 
-        "metadata": {},
-        "expected_result": FailResult,
-        "instance_variables": { 
-            'min': 4, 
-            'max': 17
-        }, 
-        "fix_value": 'hello there, this'
-    }, 
-    "LowerCase": { 
-        "input_data": 'this is all lowercase', 
-        "metadata": {},
-        "expected_result": PassResult, 
-    }, 
-    "LowerCase": { 
-        "input_data": 'OOPS, there is defintely an issue here', 
-        "metadata": {},
-        "expected_result": FailResult,
-        "fix_value": 'oops, there is defintely an issue here' 
-    }, 
-    "UpperCase": { 
-        "input_data": 'this is all lowercase', 
-        "metadata": {},
-        "expected_result": FailResult,
-        "fix_value": 'THIS IS ALL LOWERCASE' 
-    }, 
-    "UpperCase": { 
-        "input_data": 'NO ISSUE HERE', 
-        "metadata": {},
-        "expected_result": PassResult, 
-    }, 
-    "TwoWords": { 
-        "input_data": 'one TWO', 
-        "metadata": {},
-        "expected_result": PassResult, 
+        "expected_result": PassResult,
     },
-    "TwoWords": { 
-        "input_data": 'one two three four', 
-        "metadata": {},
-        "expected_result": FailResult,
-        "fix_value": 'one two' 
-    }, 
-    "OneLine": { 
-        "input_data": 'this is a simple one liner', 
-        "metadata": {},
-        "expected_result": PassResult, 
-    }, 
-    "OneLine": { 
-        "input_data": 'This should defintely fail \n since this is a new line', 
-        "metadata": {},
-        "expected_result": FailResult,
-        "fix_value": 'This should defintely fail ' 
-    }, 
-    "EndsWith": { 
-        "input_data": ['start', 'middle', 'end'], 
+    "ValidLength": {
+        "input_data": "hello there yo",
         "metadata": {},
         "expected_result": PassResult,
-        "instance_variables": { 
-            'end': 'end', 
-        } 
+        "instance_variables": {"min": 4, "max": 17},
     },
-    "EndsWith": { 
-        "input_data": ['start', 'middle', 'end'], 
+    "ValidLength": {
+        "input_data": "hello there, this is getting wordyyy",
         "metadata": {},
         "expected_result": FailResult,
-        "fix_value": ['start', 'middle', 'end', 'trunk'],
-        "instance_variables": { 
-            'end': 'trunk', 
-        } 
+        "instance_variables": {"min": 4, "max": 17},
+        "fix_value": "hello there, this",
     },
-    "ValidURL": { 
-        "input_data": 'http://google', 
+    "LowerCase": {
+        "input_data": "this is all lowercase",
         "metadata": {},
-        "expected_result": FailResult, 
-    }, 
-    "ValidURL": { 
-        "input_data": 'http://google.com', 
+        "expected_result": PassResult,
+    },
+    "LowerCase": {
+        "input_data": "OOPS, there is defintely an issue here",
         "metadata": {},
-        "expected_result": PassResult, 
-    }, 
-    "BugFreePython": { 
-        "input_data": '''def longestPalindrome(s):\n    longest_palindrome = ''\n    for i in range(len(s)):\n        
+        "expected_result": FailResult,
+        "fix_value": "oops, there is defintely an issue here",
+    },
+    "UpperCase": {
+        "input_data": "this is all lowercase",
+        "metadata": {},
+        "expected_result": FailResult,
+        "fix_value": "THIS IS ALL LOWERCASE",
+    },
+    "UpperCase": {
+        "input_data": "NO ISSUE HERE",
+        "metadata": {},
+        "expected_result": PassResult,
+    },
+    "TwoWords": {
+        "input_data": "one TWO",
+        "metadata": {},
+        "expected_result": PassResult,
+    },
+    "TwoWords": {
+        "input_data": "one two three four",
+        "metadata": {},
+        "expected_result": FailResult,
+        "fix_value": "one two",
+    },
+    "OneLine": {
+        "input_data": "this is a simple one liner",
+        "metadata": {},
+        "expected_result": PassResult,
+    },
+    "OneLine": {
+        "input_data": "This should defintely fail \n since this is a new line",
+        "metadata": {},
+        "expected_result": FailResult,
+        "fix_value": "This should defintely fail ",
+    },
+    "EndsWith": {
+        "input_data": ["start", "middle", "end"],
+        "metadata": {},
+        "expected_result": PassResult,
+        "instance_variables": {
+            "end": "end",
+        },
+    },
+    "EndsWith": {
+        "input_data": ["start", "middle", "end"],
+        "metadata": {},
+        "expected_result": FailResult,
+        "fix_value": ["start", "middle", "end", "trunk"],
+        "instance_variables": {
+            "end": "trunk",
+        },
+    },
+    "ValidURL": {
+        "input_data": "http://google",
+        "metadata": {},
+        "expected_result": FailResult,
+    },
+    "ValidURL": {
+        "input_data": "http://google.com",
+        "metadata": {},
+        "expected_result": PassResult,
+    },
+    "BugFreePython": {
+        "input_data": """def longestPalindrome(s):\n    longest_palindrome = ''\n    for i in range(len(s)):\n        
 for j in range(i, len(s)):\n            substring = s[i:j+1]\n            if substring == substring[::-1] and 
 len(substring) > len(longest_palindrome):\n                longest_palindrome = substring\n    return 
-longest_palindrome''', 
+longest_palindrome""",
         "metadata": {},
-        "expected_result": PassResult, 
-    }, 
-    "BugFreePython": { 
-        "input_data": '''deff longestPalindrome(s):\n    longest_palindrome = ''\n    for i in range(len(s)):\n        
+        "expected_result": PassResult,
+    },
+    "BugFreePython": {
+        "input_data": """deff longestPalindrome(s):\n    longest_palindrome = ''\n    for i in range(len(s)):\n        
 for j in range(i, len(s)):\n            substring = s[i:j+1]\n            if substring == substring[::-1] and 
 len(substring) > len(longest_palindrome):\n                longest_palindrome = substring\n    return 
-longest_palindrome''', 
-        "metadata": {},
-        "expected_result": FailResult, 
-    }, 
-    "ReadingTime": { 
-        "input_data": "This string is fairly short and should be able to be read in the given timeframe", 
-        "metadata": {},
-        "expected_result": PassResult,
-        "instance_variables": { 
-            'reading_time': 5, 
-        } 
-    }, 
-    "ReadingTime": { 
-        "input_data": '''This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
-            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
-            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
-            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time''', 
+longest_palindrome""",
         "metadata": {},
         "expected_result": FailResult,
-        "fix_value": '''This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
-            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
-            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
-            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time''',
-        "instance_variables": { 
-            'reading_time': 1, 
-        } 
-    }, 
-    "ValidChoices": { 
-        "input_data": "four", 
+    },
+    "ReadingTime": {
+        "input_data": "This string is fairly short and should be able to be read in the given timeframe",
         "metadata": {},
         "expected_result": PassResult,
-        "instance_variables": { 
-            'choices': ['one', 'two', 'three', 'four'], 
-        } 
-    }, 
-    "ValidChoices": { 
-        "input_data": "five", 
+        "instance_variables": {
+            "reading_time": 5,
+        },
+    },
+    "ReadingTime": {
+        "input_data": """This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
+            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
+            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
+            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time""",
         "metadata": {},
         "expected_result": FailResult,
-        "instance_variables": { 
-            'choices': ['one', 'two', 'three', 'four'], 
-        } 
-    }
+        "fix_value": """This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
+            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
+            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time
+            This string is fairly short and should be able to be read in the given timeframe but I wonder if we copy this multiple times and shorted the time frame to a fraction of the time""",
+        "instance_variables": {
+            "reading_time": 1,
+        },
+    },
+    "ValidChoices": {
+        "input_data": "four",
+        "metadata": {},
+        "expected_result": PassResult,
+        "instance_variables": {
+            "choices": ["one", "two", "three", "four"],
+        },
+    },
+    "ValidChoices": {
+        "input_data": "five",
+        "metadata": {},
+        "expected_result": FailResult,
+        "instance_variables": {
+            "choices": ["one", "two", "three", "four"],
+        },
+    },
 }
 
-validator_test_python_str = { 
-"TwoWords": { 
-        'description': 'Name for the pizza', 
-        'instructions': """
+validator_test_python_str = {
+    "TwoWords": {
+        "description": "Name for the pizza",
+        "instructions": """
 You are a helpful assistant, and you are helping me come up with a name for a pizza.
 
 ${gr.complete_string_suffix}
 """,
-        'prompt': """
+        "prompt": """
 Given the following ingredients, what would you call this pizza?
 
 ${ingredients}
-""", 
-    'prompt_params': {"ingredients": "tomato, cheese, sour cream"}, 
-    'output': 'Cheese Pizza'
+""",
+        "prompt_params": {"ingredients": "tomato, cheese, sour cream"},
+        "output": "Cheese Pizza",
     }
 }
 
-validator_test_xml = { 
+validator_test_xml = {
     "ValidLength": {
-        "expected_xml": "length: 4 17", 
-        "instance_variables": { 
-            'min': 4, 
-            'max': 17
-        }
-    }, 
-    "BugFreeSQL": { 
-        'expected_xml': 'bug-free-sql',
-    }, 
-    "ExtractedSummarySentencesMatch": { 
-        'expected_xml': 'extracted-summary-sentences-match',
+        "expected_xml": "length: 4 17",
+        "instance_variables": {"min": 4, "max": 17},
     },
-    "LowerCase": { 
-        'expected_xml': 'lower-case',
-    }, 
-    "UpperCase": { 
-        'expected_xml': 'upper-case',
-    }, 
-    "TwoWords": { 
-        'expected_xml': 'two-words',
-    }, 
-    "OneLine": { 
-        'expected_xml': 'one-line',
-    }, 
-    "ValidURL": { 
-        'expected_xml': 'valid-url',
-    }, 
-    "BugFreePython": { 
-        'expected_xml': 'bug-free-python',
-    }, 
-    "EndsWith": { 
-        'expected_xml': 'ends-with: bye',
-        "instance_variables": { 
-            'end': 'bye',
-        }
-    }, 
-    "ReadingTime": { 
-        'expected_xml': 'reading-time: 30',
-        "instance_variables": { 
-            'reading_time': 30,
-        }
-    }, 
-    "ValidChoices": { 
-        'expected_xml': "valid-choices: {['one', 'two', 'three', 'four']}",
-        "instance_variables": { 
-            'choices': ['one', 'two', 'three', 'four'], 
-        } 
-    }
+    "BugFreeSQL": {
+        "expected_xml": "bug-free-sql",
+    },
+    "ExtractedSummarySentencesMatch": {
+        "expected_xml": "extracted-summary-sentences-match",
+    },
+    "LowerCase": {
+        "expected_xml": "lower-case",
+    },
+    "UpperCase": {
+        "expected_xml": "upper-case",
+    },
+    "TwoWords": {
+        "expected_xml": "two-words",
+    },
+    "OneLine": {
+        "expected_xml": "one-line",
+    },
+    "ValidURL": {
+        "expected_xml": "valid-url",
+    },
+    "BugFreePython": {
+        "expected_xml": "bug-free-python",
+    },
+    "EndsWith": {
+        "expected_xml": "ends-with: bye",
+        "instance_variables": {
+            "end": "bye",
+        },
+    },
+    "ReadingTime": {
+        "expected_xml": "reading-time: 30",
+        "instance_variables": {
+            "reading_time": 30,
+        },
+    },
+    "ValidChoices": {
+        "expected_xml": "valid-choices: {['one', 'two', 'three', 'four']}",
+        "instance_variables": {
+            "choices": ["one", "two", "three", "four"],
+        },
+    },
 }
