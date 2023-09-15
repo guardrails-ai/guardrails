23cb3a69eb0e7df06f1d06717e1a392f298e6152
Nefertiti  Rogers <nefertitirogers@Nefertitis-MacBook-Pro.local>
2023-09-14 20:33:22 -0700

    tests for to xml and python str using pydantic

--- tests/unit_tests/test_validator_suite.py
+++ tests/unit_tests/test_validator_suite.py
@@ -1,8 +1,16 @@
-import os 
+import os
 import pytest
+import openai
+from pydantic import BaseModel, Field
+from guardrails.guard import Guard
 from typing import Dict
 import importlib
 from guardrails.validators import FailResult, PassResult
+from integration_tests.mock_llm_outputs import MockOpenAICallable
+
+'''
+    ya I'd say at a minimum for this week try to cover happy paths for validate, parse, and fix functionality
+'''
 
 # we'll start with a dictionary that maps validator name to input and expected output
 '''
@@ -132,22 +140,60 @@ longest_palindrome''',
 
 }
 
-validator_test_data = { 
-    "ExtractedSummarySentencesMatch": { 
-        'input_data': 'It was a nice day. I went to the park. I saw a dog.', 
-        'metadata': {
-        "filepaths": [
-            "./tests/unit_tests/test_assets/article1.txt",
-            "./tests/unit_tests/test_assets/article2.txt",
-        ]
-        },
-        'expected_result': PassResult
+validator_test_python_str = { 
+"TwoWords": { 
+        'description': 'Name for the pizza', 
+        'instructions': """
+You are a helpful assistant, and you are helping me come up with a name for a pizza.
+
+${gr.complete_string_suffix}
+""",
+        'prompt': """
+Given the following ingredients, what would you call this pizza?
+
+${ingredients}
+""", 
+    'prompt_params': {"ingredients": "tomato, cheese, sour cream"}, 
+    'output': 'Cheese Pizza'
     }
 }
 
+validator_test_xml = { 
+    "ValidLength": {
+        "expected_xml": "length: 4 17", 
+        "instance_variables": { 
+            'min': 4, 
+            'max': 17
+        }
+    }, 
+    "BugFreeSQL": { 
+        'expected_xml': 'bug-free-sql',
+    }, 
+    "ExtractedSummarySentencesMatch": { 
+        'expected_xml': 'extracted-summary-sentences-match',
+    },
+    "LowerCase": { 
+        'expected_xml': 'lower-case',
+    }, 
+    "UpperCase": { 
+        'expected_xml': 'upper-case',
+    }, 
+    "TwoWords": { 
+        'expected_xml': 'two-words',
+    }, 
+    "OneLine": { 
+        'expected_xml': 'one-line',
+    }, 
+    "ValidURL": { 
+        'expected_xml': 'valid-url',
+    }, 
+    "BugFreePython": { 
+        'expected_xml': 'bug-free-python',
+    }
+}
 
-#@pytest.mark.parametrize('validator_test_pass_fail', (validator_test_data))
-def run_test_suite_pass_fail(validator_test_data: Dict[str, Dict[str, str]]):
+@pytest.mark.parametrize('validator_test_data', [(validator_test_pass_fail)])
+def test_validate_pass_fail(validator_test_data: Dict[str, Dict[str, str]]):
     for validator_name in validator_test_data:
         print('testing validator: ', validator_name)
         module = importlib.import_module("guardrails.validators")
@@ -161,7 +207,7 @@ def run_test_suite_pass_fail(validator_test_data: Dict[str, Dict[str, str]]):
         #print(instance)
 
 
-def run_test_suite(validator_test_data: Dict[str, Dict[str, str]]): 
+'''def test_suite(validator_test_data: Dict[str, Dict[str, str]]):
     for validator_name in validator_test_data:
         #print('testing validator: ', validator_name)
         module = importlib.import_module("guardrails.validators")
@@ -169,14 +215,41 @@ def run_test_suite(validator_test_data: Dict[str, Dict[str, str]]):
         instance = validator_class()
         result = instance.validate(validator_test_data[validator_name]['input_data'], validator_test_data[validator_name]['metadata'])
         print('result: ', result)
-        assert isinstance(result, validator_test_data[validator_name]['expected_result'])
+        assert isinstance(result, validator_test_data[validator_name]['expected_result'])'''
+
+@pytest.mark.parametrize('validator_test_data', [(validator_test_python_str)])
+def test_validator_python_string(mocker, validator_test_data: Dict[str, Dict[str, str]]): 
+    mocker.patch("guardrails.llm_providers.OpenAICallable", new=MockOpenAICallable)
+
+    for validator_name in validator_test_data:
+        print('validator name: ', validator_name)
+        module = importlib.import_module("guardrails.validators")
+        validator_class = getattr(module, validator_name)
+        validators = [validator_class(on_fail="reask")]
+        guard = Guard.from_string(
+        validators, description=validator_test_data[validator_name]['description'], prompt=validator_test_data[validator_name]['prompt'], instructions=validator_test_data[validator_name]['instructions'],
+        )
+        _, final_output = guard(
+        llm_api=openai.Completion.create,
+        prompt_params=validator_test_data[validator_name]['prompt_params'],
+        num_reasks=1,
+        max_tokens=100,
+        )
+        assert final_output == validator_test_data[validator_name]['output']
+
+@pytest.mark.parametrize('validator_test_data', [(validator_test_xml)])
+def test_validator_to_xml(validator_test_data: Dict[str, Dict[str, str]]):
+        for validator_name in validator_test_data:
+            module = importlib.import_module("guardrails.validators")
+            validator_class = getattr(module, validator_name)
+            if 'instance_variables' in validator_test_data[validator_name]: 
+                instance = validator_class(**validator_test_data[validator_name]['instance_variables'])
+            else:
+                instance = validator_class()
+            xml = instance.to_xml_attrib()
+            assert xml == validator_test_data[validator_name]['expected_xml']
+        
 
 
-run_test_suite_pass_fail(validator_test_pass_fail)
-run_test_suite(validator_test_data)
-
-'''
-if 'metadata' in validator_test_data[validator_name]:
-            print('yes metadata exists')
-
-'''
+#run_test_suite_pass_fail(validator_test_pass_fail)
+#test_validator_python_string(validator_test_python_str)
