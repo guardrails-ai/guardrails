184891b91c08f711ad9c5fa36a617d8ac6b01b50
Caleb Courier <caleb.courier@gmail.com>
2023-09-18 11:27:27 -0500

    fix tests

--- tests/unit_tests/test_prompt.py
+++ tests/unit_tests/test_prompt.py
@@ -2,6 +2,8 @@
 
 from string import Template
 from unittest import mock
+from guardrails.prompt.instructions import Instructions
+from guardrails.prompt.prompt import Prompt
 
 import pytest
 from pydantic import BaseModel, Field
@@ -211,9 +213,7 @@ def test_reask_prompt():
 
 def test_reask_instructions():
     guard = gd.Guard.from_rail_string(RAIL_WITH_REASK_INSTRUCTIONS)
-    assert guard.output_schema._reask_instructions_template == Instructions(
-        INSTRUCTIONS
-    )
+    assert guard.output_schema._reask_instructions_template == Instructions(INSTRUCTIONS)
 
 
 @pytest.mark.parametrize(
