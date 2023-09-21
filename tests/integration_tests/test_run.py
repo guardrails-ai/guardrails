572ea8654de599aebe03816d16ab355bb9a6cc24
Nefertiti  Rogers <nefertitirogers@Nefertitis-MBP.localdomain>
2023-09-21 15:11:17 -0700

    passing tests

--- tests/integration_tests/test_run.py
+++ tests/integration_tests/test_run.py
@@ -1,3 +1,4 @@
+from guardrails.llm_providers import ( OpenAICallable, AsyncOpenAICallable )
 import pytest
 from .mock_llm_outputs import (
     MockOpenAICallable,
@@ -9,16 +10,14 @@ from lxml import etree as ET
 import guardrails as gd
 from .test_assets import  string
 
-def runner_instance(is_sync: bool):
-
-    args = {
+test_args_sync = {
         "base_model": None,
-        "instructions": ''' You are a helpful assistant, and you are helping me come up with a name for a pizza.
+        "instructions": gd.Instructions(''' You are a helpful assistant, and you are helping me come up with a name for a pizza.
             ${gr.complete_string_suffix}
-        ''',
+        '''),
         "prompt": gd.Prompt(string.COMPILED_PROMPT),
         "msg_history": None,
-        "api": MockOpenAICallable,  # Replace with a synchronous API mock
+        "api": OpenAICallable, #AsyncOpenAICallable  # Replace with a synchronous API mock
         "input_schema": None, 
         "output_schema": StringSchema(root=ET.fromstring('''<output
     type="string"
@@ -26,22 +25,37 @@ def runner_instance(is_sync: bool):
     format="two-words"
     on-fail-two-words="reask"
 />''')) , 
-        "guard_state": None 
+        "guard_state": {} 
     }
+
+test_args_async = {
+        "base_model": None,
+        "instructions": gd.Instructions(''' You are a helpful assistant, and you are helping me come up with a name for a pizza.
+            ${gr.complete_string_suffix}
+        '''),
+        "prompt": gd.Prompt(string.COMPILED_PROMPT),
+        "msg_history": None,
+        "api": AsyncOpenAICallable,  # Replace with a synchronous API mock
+        "input_schema": None, 
+        "output_schema": StringSchema(root=ET.fromstring('''<output
+    type="string"
+    description="Name for the pizza"
+    format="two-words"
+    on-fail-two-words="reask"
+/>''')) , 
+        "guard_state": {} 
+    }
+
+def runner_instance(is_sync: bool):
     if(is_sync): 
-        return Runner(**args)
+        return Runner(**test_args_sync)
     else:
-        return AsyncRunner(**args)
+        return AsyncRunner(**test_args_async)
 
 # Define a list of test cases with input values and expected outputs
 test_cases = [
     {
         "index": 1,
-        "instructions": ''' You are a helpful assistant, and you are helping me come up with a name for a pizza.
-            ${gr.complete_string_suffix}
-        ''',
-        "prompt": '''Given the following ingredients, what would you call this pizza?
-        ${ingredients}''',
         "msg_history": None,
         "api_sync": MockOpenAICallable,  # Replace with a synchronous API mock
         "api_async": MockAsyncOpenAICallable,  # Replace with an asynchronous API mock
@@ -54,26 +68,30 @@ test_cases = [
 # Create a parameterized test function
 @pytest.mark.parametrize("test_case", test_cases)
 @pytest.mark.asyncio
-async def test_sync_and_async_equivalence(test_case):
+async def test_sync_and_async_equivalence(mocker, test_case):
+
+    mocker.patch(
+        "guardrails.llm_providers.AsyncOpenAICallable",
+        new=MockAsyncOpenAICallable,
+    )
+    mocker.patch("guardrails.llm_providers.OpenAICallable", new=MockOpenAICallable)
+
     # Extract input values from the test case dictionary
     index = test_case["index"]
-    instructions = test_case["instructions"]
-    prompt = test_case["prompt"]
-    msg_history = test_case["msg_history"]
-    api_sync = test_case["api_sync"]
-    api_async = test_case["api_async"]
-    expected_output = test_case["output"]
+    instructions = test_args_sync["instructions"]
+    prompt = test_args_sync["prompt"]
+    msg_history = test_args_sync["msg_history"]
 
     # Call the 'call' method synchronously
     result_sync = runner_instance(True).call(
-        index, instructions, prompt, msg_history, api_sync, expected_output
+        index, instructions, prompt, msg_history, OpenAICallable(**{"temperature": 0}), "Tomato Cheese Pizza"
     )
 
     print('result_sync: ', result_sync)
 
     # Call the 'async_call' method asynchronously
     result_async = await runner_instance(False).async_call(
-        index, instructions, prompt, msg_history, api_async, expected_output
+        index, instructions, prompt, msg_history, AsyncOpenAICallable(**{"temperature": 0}), "Tomato Cheese Pizza"
     )
 
     print('result_async: ', result_async)
