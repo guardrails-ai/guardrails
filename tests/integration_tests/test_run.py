258965f50f7c142d55195534a29f2b8b838c5a20
Nefertiti  Rogers <nefertitirogers@Nefertitis-MBP.localdomain>
2023-09-27 15:59:40 -0700

    update tests

--- tests/integration_tests/test_run.py
+++ tests/integration_tests/test_run.py
@@ -1,74 +1,58 @@
-from guardrails.llm_providers import ( OpenAICallable, AsyncOpenAICallable )
 import pytest
-from .mock_llm_outputs import (
-    MockOpenAICallable,
-    MockAsyncOpenAICallable
-)
-from guardrails.run import Runner, AsyncRunner  # Replace 'your_module' with the actual module name
-from guardrails.schema import StringSchema
 from lxml import etree as ET
+
 import guardrails as gd
-from .test_assets import  string
+from guardrails.llm_providers import AsyncOpenAICallable, OpenAICallable
+from guardrails.run import AsyncRunner, Runner
+from guardrails.schema import StringSchema
+from guardrails.utils.logs_utils import GuardLogs
 
-test_args_sync = {
-        "base_model": None,
-        "instructions": gd.Instructions(''' You are a helpful assistant, and you are helping me come up with a name for a pizza.
-            ${gr.complete_string_suffix}
-        '''),
-        "prompt": gd.Prompt(string.COMPILED_PROMPT),
-        "msg_history": None,
-        "api": OpenAICallable, #AsyncOpenAICallable  # Replace with a synchronous API mock
-        "input_schema": None, 
-        "output_schema": StringSchema(root=ET.fromstring('''<output
+from .mock_llm_outputs import MockAsyncOpenAICallable, MockOpenAICallable
+from .test_assets import string
+
+PROMPT = gd.Prompt(string.COMPILED_PROMPT)
+INSTRUCTIONS = gd.Instructions(
+    """ You are a helpful assistant, and you are helping me
+     come up with a name for a pizza. ${gr.complete_string_suffix}"""
+)
+OUTPUT_SCHEMA = StringSchema(
+    root=ET.fromstring(
+        """<output
     type="string"
     description="Name for the pizza"
     format="two-words"
-    on-fail-two-words="reask"
-/>''')) , 
-        "guard_state": {} 
-    }
+    on-fail-two-words="reask" />"""
+    )
+)
+OUTPUT = "Tomato Cheese Pizza"
 
-test_args_async = {
-        "base_model": None,
-        "instructions": gd.Instructions(''' You are a helpful assistant, and you are helping me come up with a name for a pizza.
-            ${gr.complete_string_suffix}
-        '''),
-        "prompt": gd.Prompt(string.COMPILED_PROMPT),
-        "msg_history": None,
-        "api": AsyncOpenAICallable,  # Replace with a synchronous API mock
-        "input_schema": None, 
-        "output_schema": StringSchema(root=ET.fromstring('''<output
-    type="string"
-    description="Name for the pizza"
-    format="two-words"
-    on-fail-two-words="reask"
-/>''')) , 
-        "guard_state": {} 
-    }
 
 def runner_instance(is_sync: bool):
-    if(is_sync): 
-        return Runner(**test_args_sync)
+    if is_sync:
+        return Runner(
+            instructions=INSTRUCTIONS,
+            prompt=PROMPT,
+            msg_history=None,
+            api=OpenAICallable,
+            input_schema=None,
+            output_schema=OUTPUT_SCHEMA,
+            guard_state={},
+        )
     else:
-        return AsyncRunner(**test_args_async)
-
-# Define a list of test cases with input values and expected outputs
-test_cases = [
-    {
-        "index": 1,
-        "msg_history": None,
-        "api_sync": MockOpenAICallable,  # Replace with a synchronous API mock
-        "api_async": MockAsyncOpenAICallable,  # Replace with an asynchronous API mock
-        "output": "Tomato Cheese Pizza",  # Replace with the expected output
-    },
-    # Add more test cases as needed
-]
+        return AsyncRunner(
+            instructions=INSTRUCTIONS,
+            prompt=PROMPT,
+            msg_history=None,
+            api=AsyncOpenAICallable,
+            input_schema=None,
+            output_schema=OUTPUT_SCHEMA,
+            guard_state={},
+        )
 
 
 # Create a parameterized test function
-@pytest.mark.parametrize("test_case", test_cases)
 @pytest.mark.asyncio
-async def test_sync_and_async_equivalence(mocker, test_case):
+async def test_sync_async_call_equivalence(mocker):
 
     mocker.patch(
         "guardrails.llm_providers.AsyncOpenAICallable",
@@ -76,27 +60,91 @@ async def test_sync_and_async_equivalence(mocker, test_case):
     )
     mocker.patch("guardrails.llm_providers.OpenAICallable", new=MockOpenAICallable)
 
-    # Extract input values from the test case dictionary
-    index = test_case["index"]
-    instructions = test_args_sync["instructions"]
-    prompt = test_args_sync["prompt"]
-    msg_history = test_args_sync["msg_history"]
-
     # Call the 'call' method synchronously
     result_sync = runner_instance(True).call(
-        index, instructions, prompt, msg_history, OpenAICallable(**{"temperature": 0}), "Tomato Cheese Pizza"
+        1,
+        INSTRUCTIONS,
+        PROMPT,
+        None,
+        OpenAICallable(**{"temperature": 0}),
+        "Tomato Cheese Pizza",
     )
-
-    print('result_sync: ', result_sync)
 
     # Call the 'async_call' method asynchronously
     result_async = await runner_instance(False).async_call(
-        index, instructions, prompt, msg_history, AsyncOpenAICallable(**{"temperature": 0}), "Tomato Cheese Pizza"
+        1,
+        INSTRUCTIONS,
+        PROMPT,
+        None,
+        AsyncOpenAICallable(**{"temperature": 0}),
+        "Tomato Cheese Pizza",
     )
-
-    print('result_async: ', result_async)
 
     # Perform assertions to check if the results are equivalent
     assert result_sync.output == result_async.output
-    # Add more assertions as needed to compare other properties
 
+
+@pytest.mark.asyncio
+async def test_sync_async_validate_equivalence(mocker):
+
+    mocker.patch(
+        "guardrails.llm_providers.AsyncOpenAICallable",
+        new=MockAsyncOpenAICallable,
+    )
+    mocker.patch("guardrails.llm_providers.OpenAICallable", new=MockOpenAICallable)
+    guard_logs = GuardLogs()
+
+    parsed_output, _ = runner_instance(True).parse(1, OUTPUT, OUTPUT_SCHEMA)
+
+    # Call the 'validate' method synchronously
+    result_sync = runner_instance(True).validate(
+        guard_logs, 1, parsed_output, OUTPUT_SCHEMA
+    )
+
+    # Call the 'async_validate' method asynchronously
+    result_async = await runner_instance(False).async_validate(
+        guard_logs, 1, parsed_output, OUTPUT_SCHEMA
+    )
+
+    # Perform assertions to check if the results are equivalent
+    assert result_sync == result_async
+
+
+@pytest.mark.asyncio
+async def test_sync_async_step_equivalence(mocker):
+
+    mocker.patch(
+        "guardrails.llm_providers.AsyncOpenAICallable",
+        new=MockAsyncOpenAICallable,
+    )
+    mocker.patch("guardrails.llm_providers.OpenAICallable", new=MockOpenAICallable)
+
+    # Call the 'step' method synchronously
+    result_sync, reask_sync = runner_instance(True).step(
+        1,
+        OpenAICallable(**{"temperature": 0}),
+        INSTRUCTIONS,
+        PROMPT,
+        None,
+        {},
+        None,
+        OUTPUT_SCHEMA,
+        OUTPUT,
+    )
+
+    # Call the 'async_step' method asynchronously
+    result_async, reask_async = await runner_instance(False).async_step(
+        1,
+        AsyncOpenAICallable(**{"temperature": 0}),
+        INSTRUCTIONS,
+        PROMPT,
+        None,
+        {},
+        None,
+        OUTPUT_SCHEMA,
+        OUTPUT,
+    )
+
+    # Perform assertions to check if the results are equivalent
+    assert result_sync == result_async
+    assert reask_sync == reask_async
