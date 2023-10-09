8ae424ec417482e6edf51b1b136b818821a72b04
Karan Acharya <thekaranacharya@gmail.com>
2023-10-09 12:53:54 -0400

    Update test coverage and refactor

--- tests/unit_tests/test_validators.py
+++ tests/unit_tests/test_validators.py
@@ -30,7 +30,7 @@ from guardrails.validators import (
     register_validator,
 )
 
-from .mock_embeddings import mock_create_embedding
+from .mock_embeddings import mock_create_embedding, MOCK_EMBEDDINGS
 from .mock_provenance_v1 import mock_chat_completion, mock_chromadb_query_function
 
 
@@ -435,16 +435,9 @@ def test_similar_to_list():
     int_prev_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     str_prev_values = ["broadcom", "paypal"]
 
-    mock_embeddings = {
-        "broadcom": [0.91, 0.81, 0.21],
-        "paypal": [0.89, 0.79, 0.22],
-        "cisco": [0.9, 0.8, 0.2],  # similar example
-        "taj mahal": [0.03, 0.1, 0.11],  # dissimilar example
-    }
-
     def embed_function(text: str):
         """Mock embedding function."""
-        return mock_embeddings[text]
+        return MOCK_EMBEDDINGS[text]
 
     # Initialise Guard from string
     guard = Guard.from_string(
@@ -452,8 +445,17 @@ def test_similar_to_list():
         description="testmeout",
     )
 
-    # Test for integer values
-    # 1. Test for values within the standard deviation
+    # Check types remain intact
+    output_schema: StringSchema = guard.rail.output_schema
+    data_type: DataType = getattr(output_schema._schema, "string")
+    validators = data_type.format_attr.validators
+    validator: SimilarToList = validators[0]
+
+    assert isinstance(validator._standard_deviations, int)
+    assert isinstance(validator._threshold, float)
+
+    # 1. Test for integer values
+    # 1.1 Test for values within the standard deviation
     val = 3
     output = guard.parse(
         llm_output=val,
@@ -461,7 +463,24 @@ def test_similar_to_list():
     )
     assert int(output) == val
 
-    # 2. Test for values outside the standard deviation
+    # 1.2 Test not passing prev_values
+    # Should raise ValueError
+    with pytest.raises(ValueError):
+        val = 3
+        output = guard.parse(
+            llm_output=val,
+        )
+
+    # 1.3 Test passing str prev values for int val
+    # Should raise ValueError
+    with pytest.raises(ValueError):
+        val = 3
+        output = guard.parse(
+            llm_output=val,
+            metadata={"prev_values": [str(i) for i in int_prev_values]},
+        )
+
+    # 1.4 Test for values outside the standard deviation
     val = 300
     output = guard.parse(
         llm_output=val,
@@ -469,8 +488,8 @@ def test_similar_to_list():
     )
     assert output is None
 
-    # Test for string values
-    # 1. Test for values within the standard deviation
+    # 2. Test for string values
+    # 2.1 Test for values within the standard deviation
     val = "cisco"
     output = guard.parse(
         llm_output=val,
@@ -478,7 +497,34 @@ def test_similar_to_list():
     )
     assert output == val
 
-    # 2. Test for values outside the standard deviation
+    # 2.2 Test not passing prev_values
+    # Should raise ValueError
+    with pytest.raises(ValueError):
+        val = "cisco"
+        output = guard.parse(
+            llm_output=val,
+            metadata={"embed_function": embed_function},
+        )
+
+    # 2.3 Test passing int prev values for str val
+    # Should raise ValueError
+    with pytest.raises(ValueError):
+        val = "cisco"
+        output = guard.parse(
+            llm_output=val,
+            metadata={"prev_values": int_prev_values, "embed_function": embed_function},
+        )
+
+    # 2.4 Test not pasisng embed_function
+    # Should raise ValueError
+    with pytest.raises(ValueError):
+        val = "cisco"
+        output = guard.parse(
+            llm_output=val,
+            metadata={"prev_values": str_prev_values},
+        )
+
+    # 2.5 Test for values outside the standard deviation
     val = "taj mahal"
     output = guard.parse(
         llm_output=val,
