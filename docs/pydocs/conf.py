d996567822d20c056411af1756d252f32afd3320
zsimjee <zaydsimjee@gmail.com>
2023-10-13 10:28:33 -0700

    figured out autodoc format

--- docs/pydocs/conf.py
+++ docs/pydocs/conf.py
@@ -24,3 +24,10 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_theme_options = {
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
---
+++ docs/pydocs/datatypes.rst
@@ -0,0 +1,7 @@
+Data Types
+==========
+  .. automodule:: guardrails.datatypes
+    :members:
+    :private-members:
+
+.. :members: get_validators, registry, DataType, register_type, Scalar, set_children, validate, from_str, from_xml, model, validators, to_object_element
