d472fb7a674ffdba9f26aecda1526d81e14cf8d8
Rafael Irgolic <hello@irgolic.com>
2023-11-02 15:27:59 +0000

    Remove deprecated datatypes from pydantic

--- guardrails/utils/pydantic_utils.py
+++ guardrails/utils/pydantic_utils.py
@@ -8,10 +8,9 @@ from typing import Any, Callable, Dict, Optional, Type, Union, get_args, get_ori
 
 from griffe.dataclasses import Docstring
 from griffe.docstrings.parsers import Parser, parse
-from pydantic import BaseModel, HttpUrl, validator
+from pydantic import BaseModel, validator
 from pydantic.fields import ModelField
 
-from guardrails.datatypes import URL as URLDataType
 from guardrails.datatypes import Boolean as BooleanDataType
 from guardrails.datatypes import Case as CaseDataType
 from guardrails.datatypes import Choice
@@ -22,7 +21,6 @@ from guardrails.datatypes import Float as FloatDataType
 from guardrails.datatypes import Integer as IntegerDataType
 from guardrails.datatypes import List as ListDataType
 from guardrails.datatypes import Object as ObjectDataType
-from guardrails.datatypes import PythonCode as PythonCodeDataType
 from guardrails.datatypes import String as StringDataType
 from guardrails.datatypes import Time as TimeDataType
 from guardrails.formatattr import FormatAttr
@@ -293,9 +291,6 @@ def field_to_datatype(field: Union[ModelField, Type]) -> Type[DataType]:
     # Get the type annotation from the type_annotation
     type_annotation = prepare_type_annotation(field)
 
-    # Use inline import to avoid circular dependency
-    from guardrails.datatypes import PythonCode
-
     # Map the type annotation to the corresponding field type
     if is_list(type_annotation):
         return ListDataType
@@ -313,12 +308,8 @@ def field_to_datatype(field: Union[ModelField, Type]) -> Type[DataType]:
         return StringDataType
     elif type_annotation == time:
         return TimeDataType
-    elif type_annotation == HttpUrl:
-        return URLDataType
     elif typing.get_origin(type_annotation) == Union:
         return ChoiceDataType
-    elif type_annotation == PythonCode:
-        return PythonCodeDataType
     else:
         raise ValueError(f"Unsupported type: {type_annotation}")
 
