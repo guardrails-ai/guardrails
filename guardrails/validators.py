5e85ffd283d5f2462e8b130ce7fe49956ee1f744
Karan Acharya <karan@guardrailsai.com>
2023-11-03 13:11:15 -0400

    Remove list type casting

--- guardrails/validators.py
+++ guardrails/validators.py
@@ -2493,8 +2493,8 @@ class ToxicLanguage(Validator):
         # with confidence higher than the threshold
         pred_labels = []
         if value:
-            results = list(self._detoxify_pipeline(value))
-            if results:
+            results = self._detoxify_pipeline(value)
+            if results and results[0]:
                 for label_info in results[0]:
                     label, score = label_info["label"], label_info["score"]
                     if label in self._labels and score > self._threshold:
