7edb9de11e2aad62e120e8fe6a00e71690f147d0
Karan Acharya <karan@guardrailsai.com>
2023-11-08 17:31:53 -0500

    Change func names and add v0 support for OpenAI chat models

--- guardrails/llm_providers.py
+++ guardrails/llm_providers.py
@@ -25,7 +25,7 @@ class PromptCallableException(Exception):
     pass
 
 
-def get_openai_token_count(text: str, model_name: str) -> int:
+def num_tokens_from_string(text: str, model_name: str) -> int:
     """Returns the number of tokens in a text string.
 
     Supported for OpenAI models only. This is a helper function
@@ -42,6 +42,62 @@ def get_openai_token_count(text: str, model_name: str) -> int:
     """
     encoding = tiktoken.encoding_for_model(model_name)
     num_tokens = len(encoding.encode(text))
+    return num_tokens
+
+
+def num_tokens_from_messages(
+    messages: List[Dict[str, str]], model: str = "gpt-3.5-turbo-0613"
+) -> int:
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+    }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = (
+            4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        )
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print(
+            """Warning: gpt-3.5-turbo may update over time.
+            Returning num tokens assuming gpt-3.5-turbo-0613."""
+        )
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print(
+            """Warning: gpt-4 may update over time.
+            Returning num tokens assuming gpt-4-0613."""
+        )
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}.
+            See https://github.com/openai/openai-python/blob/main/chatml.md for
+            information on how messages are converted to tokens."""
+        )
+
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+
+    # every reply is primed with <|start|>assistant<|message|>
+    num_tokens += 3
     return num_tokens
 
 
@@ -161,11 +217,11 @@ class OpenAICallable(PromptCallableBase):
 
             # Also, it no longer returns usage information
             # So manually count the tokens using tiktoken
-            prompt_token_count = get_openai_token_count(
+            prompt_token_count = num_tokens_from_string(
                 text=nonchat_prompt(prompt=text, instructions=instructions),
                 model_name=engine,
             )
-            response_token_count = get_openai_token_count(
+            response_token_count = num_tokens_from_string(
                 text=complete_output, model_name=engine
             )
 
@@ -235,23 +291,61 @@ class OpenAIChatCallable(PromptCallableBase):
             **kwargs,
         )
 
-        # Extract string from response
-        if "function_call" in openai_response["choices"][0]["message"]:  # type: ignore
-            output = openai_response["choices"][0]["message"][  # type: ignore
-                "function_call"
-            ]["arguments"]
-        else:
-            output = openai_response["choices"][0]["message"]["content"]  # type: ignore
+        # Check if kwargs stream is passed in
+        if kwargs.get("stream", None) is None:
+            # If stream is not defined, return default behavior
+            # Extract string from response
+            if (
+                "function_call" in openai_response["choices"][0]["message"]
+            ):  # type: ignore
+                output = openai_response["choices"][0]["message"][  # type: ignore
+                    "function_call"
+                ]["arguments"]
+            else:
+                output = openai_response["choices"][0]["message"][
+                    "content"
+                ]  # type: ignore
 
-        return LLMResponse(
-            output=output,
-            prompt_token_count=openai_response["usage"][  # type: ignore
-                "prompt_tokens"
-            ],
-            response_token_count=openai_response["usage"][  # type: ignore
-                "completion_tokens"
-            ],
-        )
+            return LLMResponse(
+                output=output,
+                prompt_token_count=openai_response["usage"][  # type: ignore
+                    "prompt_tokens"
+                ],
+                response_token_count=openai_response["usage"][  # type: ignore
+                    "completion_tokens"
+                ],
+            )
+        else:
+            # If stream is defined, openai returns a generator
+            # that we need to iterate through
+            collected_messages = []
+            # iterate through the stream of events
+            for chunk in openai_response:
+                chunk_message = chunk["choices"][0]["delta"]  # extract the message
+                collected_messages.append(chunk_message)  # save the message
+
+            complete_output = "".join(
+                [msg.get("content", "") for msg in collected_messages]
+            )
+
+            # Also, it no longer returns usage information
+            # So manually count the tokens using tiktoken
+            prompt_token_count = num_tokens_from_messages(
+                messages=chat_prompt(
+                    prompt=text, instructions=instructions, msg_history=msg_history
+                ),
+                model=model,
+            )
+            response_token_count = num_tokens_from_string(
+                text=complete_output, model_name=model
+            )
+
+            # Return the LLMResponse
+            return LLMResponse(
+                output=complete_output,
+                prompt_token_count=prompt_token_count,
+                response_token_count=response_token_count,
+            )
 
 
 class ManifestCallable(PromptCallableBase):
