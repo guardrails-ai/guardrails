0a2f1b4e55ddb50e959271701e94d83257f34ddd
Karan Acharya <karan@guardrailsai.com>
2023-11-15 19:39:31 -0500

    Add new attribute in LLMResponse

--- guardrails/utils/logs_utils.py
+++ guardrails/utils/logs_utils.py
@@ -40,6 +40,7 @@ class LLMResponse(ArbitraryModel):
     prompt_token_count: Optional[int] = None
     response_token_count: Optional[int] = None
     output: str
+    stream_output: Optional[Any] = None
 
 
 class GuardLogs(ArbitraryModel):
