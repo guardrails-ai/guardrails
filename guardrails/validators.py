a0b604a2144349d91b33aea18405f7489a91068f
Karan Acharya <karan@guardrailsai.com>
2023-11-17 17:44:25 -0500

    Strong type results to covaraiant Sequence, as suggested by Pyright

--- guardrails/validators.py
+++ guardrails/validators.py
@@ -13,7 +13,7 @@ import re
 import string
 import warnings
 from functools import partial
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union, cast, Sequence
 
 import openai
 import rstr
@@ -2149,6 +2149,7 @@ class PIIFilter(Validator):
             anonymized_text (str): The anonymized text.
         """
         results = self.pii_analyzer.analyze(text=text, entities=entities, language="en")
+        results = cast(Sequence[Any], results)
         anonymized_text = self.pii_anonymizer.anonymize(
             text=text, analyzer_results=results
         ).text
