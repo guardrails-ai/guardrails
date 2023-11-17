5f03aae6cdcf7e77a95646309a0d60ec143d2f73
Karan Acharya <karan@guardrailsai.com>
2023-11-17 18:14:11 -0500

    Create PII Filter validator (#395)
    * Add PII filter: v0
    
    * Update PII filter
    
    * Update class docstring, add more entities to filter, bugfix and update some comments
    
    * Move logic to helper function, update DEV_REQUIREMENTS, add integration tests
    
    * Fix linting
    
    * Write code according to Python 3.9
    
    * Add mocks for AnalyzerEngine and AnonymizerEngine
    
    * Change | to Union
    
    * Add notebook example demo for PIIFilter
    
    * Add package imports
    
    * Init pydantic_utils v2
    
    * Adjust tests for pydantic2
    
    * pydantic: Allow Dict/List field types (fix #319)
    
    * test_validators: Fix SimilarToList validator test
    
    * CI: handle pydantic v1 and v2 separately
    
    * list => List
    
    * parsing_utils: Type ignore
    
    * pydantic_utils/v2: safe BareModel
    
    * fix Makefile for poetry
    
    * fix ci cache for pydantic versions
    
    * Remove setup.py
    
    * Update pyproject and poetry
    
    * Linting fixes
    
    * Remove types remain intact changes
    
    * Strong type results to covaraiant Sequence, as suggested by Pyright
    
    * Fix linting issues
    
    * Change casting
    
    * Add else condition for pii_entities to avoid unbound errors for entities_to_filter
    
    ---------
    
    Co-authored-by: zsimjee <zaydsimjee@gmail.com>
    Co-authored-by: Rafael Irgolic <hello@irgolic.com>

--- guardrails/validators.py
+++ guardrails/validators.py
@@ -13,7 +13,7 @@ import re
 import string
 import warnings
 from functools import partial
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union, cast
 
 import openai
 import rstr
@@ -41,7 +41,14 @@ else:
 try:
     import detect_secrets  # type: ignore
 except ImportError:
-    detect_secrets = None  # type: ignore
+    detect_secrets = None
+
+try:
+    from presidio_analyzer import AnalyzerEngine
+    from presidio_anonymizer import AnonymizerEngine
+except ImportError:
+    AnalyzerEngine = None
+    AnonymizerEngine = None
 
 try:
     import nltk  # type: ignore
@@ -2065,6 +2072,130 @@ class ProvenanceV1(Validator):
             raise ValueError("distance_metric must be 'cosine'.")
 
         return top_chunks
+
+
+@register_validator(name="pii", data_type="string")
+class PIIFilter(Validator):
+    """Validates that any text does not contain any PII.
+
+    This validator uses Microsoft's Presidio (https://github.com/microsoft/presidio)
+    to detect PII in the text. If PII is detected, the validator will fail with a
+    programmatic fix that anonymizes the text. Otherwise, the validator will pass.
+
+    **Key Properties**
+
+    | Property                      | Description                         |
+    | ----------------------------- | ----------------------------------- |
+    | Name for `format` attribute   | `pii`                               |
+    | Supported data types          | `string`                            |
+    | Programmatic fix              | Anonymized text with PII filtered   |
+
+    Parameters: Arguments
+        pii_entities (str | List[str], optional): The PII entities to filter. Must be
+            one of `pii` or `spi`. Defaults to None. Can also be set in metadata.
+    """
+
+    PII_ENTITIES_MAP = {
+        "pii": [
+            "EMAIL_ADDRESS",
+            "PHONE_NUMBER",
+            "DOMAIN_NAME",
+            "IP_ADDRESS",
+            "DATE_TIME",
+            "LOCATION",
+            "PERSON",
+            "URL",
+        ],
+        "spi": [
+            "CREDIT_CARD",
+            "CRYPTO",
+            "IBAN_CODE",
+            "NRP",
+            "MEDICAL_LICENSE",
+            "US_BANK_NUMBER",
+            "US_DRIVER_LICENSE",
+            "US_ITIN",
+            "US_PASSPORT",
+            "US_SSN",
+        ],
+    }
+
+    def __init__(
+        self,
+        pii_entities: Union[str, List[str], None] = None,
+        on_fail: Union[Callable[..., Any], None] = None,
+        **kwargs,
+    ):
+        if AnalyzerEngine is None or AnonymizerEngine is None:
+            raise ImportError(
+                "You must install the `presidio-analyzer`, `presidio-anonymizer`"
+                "and a spaCy language model to use the PII validator."
+                "Refer to https://microsoft.github.io/presidio/installation/"
+            )
+
+        super().__init__(on_fail, pii_entities=pii_entities, **kwargs)
+        self.pii_entities = pii_entities
+        self.pii_analyzer = AnalyzerEngine()
+        self.pii_anonymizer = AnonymizerEngine()
+
+    def get_anonymized_text(self, text: str, entities: List[str]) -> str:
+        """Analyze and anonymize the text for PII.
+
+        Args:
+            text (str): The text to analyze.
+            pii_entities (List[str]): The PII entities to filter.
+
+        Returns:
+            anonymized_text (str): The anonymized text.
+        """
+        results = self.pii_analyzer.analyze(text=text, entities=entities, language="en")
+        results = cast(List[Any], results)
+        anonymized_text = self.pii_anonymizer.anonymize(
+            text=text, analyzer_results=results
+        ).text
+        return anonymized_text
+
+    def validate(self, value: Any, metadata: Dict[str, Any]) -> ValidationResult:
+        # Entities to filter passed through metadata take precedence
+        pii_entities = metadata.get("pii_entities", self.pii_entities)
+        if pii_entities is None:
+            raise ValueError(
+                "`pii_entities` must be set in order to use the `PIIFilter` validator."
+                "Add this: `pii_entities=['PERSON', 'PHONE_NUMBER']`"
+                "OR pii_entities='pii' or 'spi'"
+                "in init or metadata."
+            )
+
+        # Check that pii_entities is a string OR list of strings
+        if isinstance(pii_entities, str):
+            # A key to the PII_ENTITIES_MAP
+            entities_to_filter = self.PII_ENTITIES_MAP.get(pii_entities, None)
+            if entities_to_filter is None:
+                raise ValueError(
+                    f"`pii_entities` must be one of {self.PII_ENTITIES_MAP.keys()}"
+                )
+        elif isinstance(pii_entities, list):
+            entities_to_filter = pii_entities
+        else:
+            raise ValueError(
+                f"`pii_entities` must be one of {self.PII_ENTITIES_MAP.keys()}"
+                " or a list of strings."
+            )
+
+        # Analyze the text, and anonymize it if there is PII
+        anonymized_text = self.get_anonymized_text(
+            text=value, entities=entities_to_filter
+        )
+
+        # If anonymized value text is different from original value, then there is PII
+        if anonymized_text != value:
+            return FailResult(
+                error_message=(
+                    f"The following text in your response contains PII:\n{value}"
+                ),
+                fix_value=anonymized_text,
+            )
+        return PassResult()
 
 
 @register_validator(name="similar-to-list", data_type="string")
