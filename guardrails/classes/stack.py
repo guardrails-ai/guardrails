aa136e530f856308f97e81db3eb82d97d70863a0
Caleb Courier <caleb.courier@gmail.com>
2023-11-21 08:50:28 -0600

    autoformat

--- guardrails/classes/stack.py
+++ guardrails/classes/stack.py
@@ -11,35 +11,38 @@ class Stack(List[T]):
         """Tests if this stack is empty."""
         return len(self) == 0
 
-    
     def peek(self) -> Optional[T]:
-        """Looks at the object at the top (last/most recently added) of this stack without removing it from the stack."""
+        """Looks at the object at the top (last/most recently added) of this
+        stack without removing it from the stack."""
         return self.at(-1)
 
     def pop(self) -> Optional[T]:
-        """Removes the object at the top of this stack and returns that object as the value of this function."""
+        """Removes the object at the top of this stack and returns that object
+        as the value of this function."""
         try:
             value = super().pop()
             return value
         except IndexError:
             pass
-    
-    def push(self, item: T):
-        """
-        Pushes an item onto the top of this stack.
+
+    def push(self, item: T) -> None:
+        """Pushes an item onto the top of this stack.
+
         Proxy of List.append
         """
         self.append(item)
 
     def search(self, x: T) -> Optional[int]:
-        """
-        Returns the 0-based position of the last item whose value is equal to x on this stack.
-        We deviate from the typical 1-based position, because most python users (and developers in general) are accustomed to 0-based indexing.
-        len(self) - self.copy().reverse().index(x)
+        """Returns the 0-based position of the last item whose value is equal
+        to x on this stack.
+
+        We deviate from the typical 1-based position used by Stack classes (i.e. Java)
+        because most python users (and developers in general) are accustomed
+        to 0-based indexing.
         """
         copy = self.copy()
         copy.reverse()
-        
+
         try:
             found_index = copy.index(x)
             return len(self) - found_index - 1
@@ -47,8 +50,10 @@ class Stack(List[T]):
             pass
 
     def at(self, index: int) -> T:
-        """
-        Returns the item located at the index.  If the index does not exist in the stack (Overflow or Underflow), None is returned instead.
+        """Returns the item located at the index.
+
+        If the index does not exist in the stack (Overflow or
+        Underflow), None is returned instead.
         """
         try:
             value = self[index]
@@ -58,39 +63,37 @@ class Stack(List[T]):
 
     @property
     def first(self) -> T:
-        """
-        Returns the first item of the stack without removing it.
+        """Returns the first item of the stack without removing it.
+
         Same as Stack.bottom.
         """
         return self.at(0)
 
     @property
     def last(self) -> T:
-        """
-        Returns the last item of the stack without removing it.
+        """Returns the last item of the stack without removing it.
+
         Same as Stack.top.
         """
         return self.at(-1)
 
     @property
     def bottom(self) -> T:
-        """
-        Returns the item on the bottom of the stack without removing it.
+        """Returns the item on the bottom of the stack without removing it.
+
         Same as Stack.first.
         """
         return self.at(0)
 
     @property
     def top(self) -> T:
-        """
-        Returns the item on the top of the stack without removing it.
+        """Returns the item on the top of the stack without removing it.
+
         Same as Stack.last.
         """
         return self.at(-1)
 
     @property
     def length(self) -> T:
-        """
-        Returns the number of items in the Stack.
-        """
+        """Returns the number of items in the Stack."""
         return len(self)
