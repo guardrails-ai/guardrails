41487df73b849033a17b051edca4e1606d4c297b
Caleb Courier <caleb.courier@gmail.com>
2024-01-09 15:26:25 -0600

    write to rc file

--- guardrails/cli/configure.py
+++ guardrails/cli/configure.py
@@ -1,12 +1,23 @@
+import os
 import typer
 
+from os.path import expanduser
 from typing import Optional
 from guardrails.cli.guardrails import guardrails
 from guardrails.cli.logger import logger
 
 
 def save_configuration_file(client_id: str, client_secret: str, no_metrics: bool) -> None:
-    return
+    home = expanduser("~")
+    guardrails_rc = os.path.join(home, '.guardrailsrc')
+    with open(guardrails_rc, 'w') as rc_file:
+        lines = [
+            f"client_id={client_id}{os.linesep}",
+            f"client_secret={client_secret}{os.linesep}",
+            f"no_metrics={str(no_metrics).lower()}{os.linesep}",
+        ]
+        rc_file.writelines(lines)
+        rc_file.close()
 
 
 @guardrails.command()
