dcfad5d6dfce210c10766bc1bd68af08914cd444
Zayd Simjee <zaydsimjee@gmail.com>
2024-02-05 08:42:25 -0800

    WIP server

--- async-tester.py
+++ async-tester.py
@@ -1,32 +1,35 @@
+from flask_cors import CORS, cross_origin
 from guardrails import Guard, validators
 import openai
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
+import logging 
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 # create flask app
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": "*"}}, expose_headers=['Access-Control-Allow-Origin', 'Content-Type'])
+
+@app.route('/', methods=['GET'])
+def home():
+  return "Hello, World!"
 
 # create a route that returns the guard execution results
-@app.route('/validate', methods=['GET'])
+@app.route('/validate', methods=['POST', 'OPTIONS'])
+@cross_origin()
 def validate():
   # extract format string from query parameters
-  format_string = request.args.get('format')
-  prompt = request.args.get('prompt')
-  num_reasks = int(request.args.get('num_reasks') or 0)
-  output = f"""<output type='string' format='{format_string}' on_fail='reask'/> """
+  validator = request.json['validator']
+  validatorArgs = request.json['validatorArgs']
+  prompt = request.json['prompt']
+  num_reasks = int(request.json['num_reasks'] or 0)
+
+  validatorImport = getattr(validators, validator)
 
   guard = Guard.from_string(
-    validators=[validators.RegexMatch('W.*', on_fail='reask', )],
-    reask_prompt='Please begin the answer with the letter "W".' + prompt,
-    reask_instructions='Please begin the answer with the letter "W".',
+    validators=[validatorImport(**validatorArgs)],
   )
-  #   f"""
-  #   <rail version="0.1">
-  #     <output type='string' format="{format_string}" on_fail='reask'/>
-  #     <prompt>{prompt}</prompt>
-  #   </rail>
-  #   """
-  # )
 
   guard(
     llm_api=openai.chat.completions.create,
@@ -34,17 +37,27 @@ def validate():
     prompt=prompt,
   )
 
-
-  return {
+  response_obj = {
     "validated_output": guard.history.last.validated_output,
     "failed_validations": guard.history.last.failed_validations.length,
     "passed_num_reasks": num_reasks,
     "raw_output": guard.history.last.raw_outputs,
     "validation": guard.history.last.validator_logs.length,
-    "output_style": output,
     "compiled_prompt": guard.history.last.compiled_prompt,
     "compiled_instructions": guard.history.last.compiled_instructions,
   }
+  # make a new response object with headers
+  # response = app.response_class(
+  #   response=jsonify(response_obj),
+  #   status=200,
+  #   mimetype='application/json',
+  #   content_type='application/json',
+  #   headers={
+  #     'Access-Control-Allow-Origin': '*',
+  #     'Content-Type': 'application/json'
+  #   }
+  # )
+  return jsonify(response_obj)
 
 # run the app
 app.run(debug=True)
