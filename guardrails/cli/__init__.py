bb8ed355c19ab435c372198b4591c024c5764519
Caleb Courier <caleb.courier@gmail.com>
2024-02-06 17:19:22 -0800

    lint fix

--- guardrails/cli/__init__.py
+++ guardrails/cli/__init__.py
@@ -3,7 +3,9 @@ import guardrails.cli.validate  # noqa
 from guardrails.cli.guardrails import guardrails as cli
 from guardrails.cli.hub import hub
 
-cli.add_typer(hub, name="hub", help="Manage validators installed from the Guardrails Hub.")
+cli.add_typer(
+    hub, name="hub", help="Manage validators installed from the Guardrails Hub."
+)
 
 
 if __name__ == "__main__":
