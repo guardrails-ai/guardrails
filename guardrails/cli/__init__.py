56e3882bb7fff4bd18bbf27b35b9603e50b82368
Zayd Simjee <zaydsimjee@gmail.com>
2024-02-06 16:43:47 -0800

    remove compile, add help for hub

--- guardrails/cli/__init__.py
+++ guardrails/cli/__init__.py
@@ -1,10 +1,9 @@
-import guardrails.cli.compile  # noqa
 import guardrails.cli.configure  # noqa
 import guardrails.cli.validate  # noqa
 from guardrails.cli.guardrails import guardrails as cli
 from guardrails.cli.hub import hub
 
-cli.add_typer(hub, name="hub")
+cli.add_typer(hub, name="hub", help="Manage validators installed from the Guardrails Hub.")
 
 
 if __name__ == "__main__":
