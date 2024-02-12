f16bb5c11ec33741690c76a390c2ce2113ba6aa4
Caleb Courier <caleb.courier@gmail.com>
2024-02-12 09:55:11 -0600

    impl create, start submit

--- guardrails/cli/hub/__init__.py
+++ guardrails/cli/hub/__init__.py
@@ -1,2 +1,3 @@
 import guardrails.cli.hub.install  # noqa
+import guardrails.cli.hub.create_validator  # noqa
 from guardrails.cli.hub.hub import hub  # noqa
--- guardrails/cli/hub/create_validator.py
+++ guardrails/cli/hub/create_validator.py
@@ -1,13 +1,12 @@
 import os
-from pydash import snake_case
+from pydash import snake_case, pascal_case
 import typer
-from os.path import expanduser
 from guardrails.cli.hub.hub import hub
+from guardrails.cli.logger import LEVELS, logger
 from string import Template
 
-validator_template = Template("""from typing import Any, Dict
+validator_template = Template("""from typing import Any, Callable, Dict, Optional
 
-from guardrails.logger import logger
 from guardrails.validator_base import (
     FailResult,
     PassResult,
@@ -16,49 +15,69 @@ from guardrails.validator_base import (
     register_validator,
 )
 
+# List any additional dependencies here.
+\"""
+dependencies = [                              
+    "guardrails-ai>=0.3.2"
+]
+                              
+[project.optional-dependencies]
+dev = [
+    "pytest"
+] 
+\"""
+                              
 
-@register_validator(name="guardrails/${package_name}", data_type=["string", "list"])
+@register_validator(name="guardrails/${package_name}", data_type="string")
 class ${class_name}(Validator):
-    \"""Validates that a string or list ends with a given value.
+    \"""Validates that {fill in how you validator interacts with the passed value}.
 
     **Key Properties**
 
-    | Property                      | Description                        |
-    | ----------------------------- | ---------------------------------  |
-    | Name for `format` attribute   | `hub://guardrails/${package_name}` |
-    | Supported data types          | `string`, `list`                   |
-    | Programmatic fix              | Append the given value to the end. |
+    | Property                      | Description                       |
+    | ----------------------------- | --------------------------------- |
+    | Name for `format` attribute   | `guardrails/${package_name}`   |
+    | Supported data types          | `string`                          |
+    | Programmatic fix              | {If you support programmatic fixes, explain it here. Otherwise `None`} |
 
     Args:
-        end: The required last element.
-    \"""
+        arg_1 (string): {Description of the argument here}
+        arg_2 (string): {Description of the argument here}
+    \"""  # noqa
 
-    def __init__(self, end: str, on_fail: str = "fix"):
-        super().__init__(on_fail=on_fail, end=end)
-        self._end = end
+    # If you don't have any init args, you can omit the __init__ method.
+    def __init__(
+        self,
+        arg_1: str,
+        arg_2: str,
+        on_fail: Optional[Callable] = None,
+    ):
+        super().__init__(on_fail=on_fail, arg_1=arg_1, arg_2=arg_2)
+        self._arg_1 = arg_1
+        self._arg_2 = arg_2
 
     def validate(self, value: Any, metadata: Dict) -> ValidationResult:
-        logger.debug(f"Validating {value} ends with {self._end}...")
-
-        if not value[-1] == self._end:
+        \"""Validates that {fill in how you validator interacts with the passed value}.\"""
+        # Add your custom validator logic here and return a PassResult or FailResult accordingly.
+        if value != "pass": # FIXME
             return FailResult(
-                error_message=f"{value} must end with {self._end}",
-                fix_value=value + [self._end],
+                error_message="{A descriptive but concise error message about why validation failed}",
+                fix_value="{The programmtic fix if applicable, otherwise remove this kwarg.}",
             )
-
         return PassResult()
 
-                              
+
+# Run tests via `pytest -rP ${filepath}`
 class Test${class_name}:
     def test_success_case(self):
         validator = ${class_name}("s")
-        result = validator.validate("pass")
-        assert isintance(result, PassResult) is True
+        result = validator.validate("pass", {})
+        assert isinstance(result, PassResult) is True
 
     def test_failure_case(self):
         validator = ${class_name}("s")
-        result = validator.validate("fail")
-        assert isintance(result, FailResult) is True
+        result = validator.validate("fail", {})
+        assert isinstance(result, FailResult) is True
         assert result.error_message == "fail must end with s"
         assert result.fix_value == "fails"
 """)
@@ -74,6 +93,33 @@ def create_validator(
         default="./{validator_name}.py"
     )
 ):
-    file_name = snake_case(name)
-    target = os.path.join(os.getcwd(), file_name)
+    package_name = snake_case(name)
+    class_name = pascal_case(name)
+    if not filepath or filepath == "./{validator_name}.py":
+        filepath = f"./{package_name}.py"
+
+    template = validator_template.safe_substitute({
+        "package_name": package_name,
+        "class_name": class_name,
+        "filepath": filepath
+    })
+    
+    target = os.path.abspath(filepath)
+    with open(target, 'w') as validator_file:
+        validator_file.write(template)
+        validator_file.close()
+
+    success_message = Template(
+        """
+
+    Successfully created validator template at ${filepath}!
+
+    Make any necessary changes then submit for review with the following command:
+
+    guardrails hub submit ${package_name} ${filepath}
+    """
+    ).safe_substitute(
+        {"filepath": filepath, "package_name": package_name}
+    )
+    logger.log(level=LEVELS.get("SUCCESS"), msg=success_message)  # type: ignore
     
