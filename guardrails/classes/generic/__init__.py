75b10e837bd516d790dcafe52734d92f9a87fe37
Caleb Courier <caleb.courier@gmail.com>
2024-02-14 17:07:32 -0600

    lint fix

--- guardrails/classes/generic/__init__.py
+++ guardrails/classes/generic/__init__.py
@@ -1,4 +1,4 @@
-from guardrails.classes.generic.stack import Stack
 from guardrails.classes.generic.serializeable import Serializeable
+from guardrails.classes.generic.stack import Stack
 
 __all__ = ["Stack", "Serializeable"]
