0d99ed48a79d60cda9c7beaa29a1c5796ca1b405
Karan Acharya <karan@guardrailsai.com>
2024-03-05 11:37:49 -0500

    Lint

--- tests/integration_tests/test_litellm.py
+++ tests/integration_tests/test_litellm.py
@@ -1,8 +1,10 @@
+import importlib.util
 from dataclasses import dataclass
+
+import pytest
+
 import guardrails as gd
 from guardrails.validators import LowerCase
-import importlib.util
-import pytest
 
 
 # Mock the litellm.completion function and
@@ -11,19 +13,23 @@ import pytest
 class Message:
     content: str
 
+
 @dataclass
 class Choice:
     message: Message
+
 
 @dataclass
 class Usage:
     prompt_tokens: int
     completion_tokens: int
 
+
 @dataclass
 class MockResponse:
     choices: list[Choice]
     usage: Usage
+
 
 class MockCompletion:
     @staticmethod
@@ -32,10 +38,12 @@ class MockCompletion:
             choices=[Choice(message=Message(content="GUARDRAILS AI"))],
             usage=Usage(prompt_tokens=10, completion_tokens=20),
         )
-    
+
+
 TEST_PROMPT = "Suggest a name for an AI company. The name should be short and catchy."
 guard = gd.Guard.from_string(validators=[LowerCase(on_fail="fix")], prompt=TEST_PROMPT)
-    
+
+
 @pytest.mark.skipif(
     not importlib.util.find_spec("litellm"),
     reason="`litellm` is not installed",
@@ -43,9 +51,9 @@ guard = gd.Guard.from_string(validators=[LowerCase(on_fail="fix")], prompt=TEST_
 def test_litellm_completion(mocker):
     """Test that Guardrails can use litellm for completions."""
     import litellm
+
     mocker.patch("litellm.completion", return_value=MockCompletion.create())
 
     raw, validated, *rest = guard(litellm.completion)
     assert raw == "GUARDRAILS AI"
     assert validated == "guardrails ai"
-
