ca7ae2559db105844f0286a4b7494d28ecc0e724
Zayd Simjee <zaydsimjee@gmail.com>
2024-03-18 14:08:44 -0700

    check for token expiration

--- tests/unit_tests/cli/server/test_hub_client.py
+++ tests/unit_tests/cli/server/test_hub_client.py
@@ -1,4 +1,14 @@
+import datetime
+
 import pytest
+from jwt import JWT, jwk_from_dict
+
+from guardrails.classes.credentials import Credentials
+from guardrails.cli.server.hub_client import (
+    TOKEN_EXPIRED_MESSAGE,
+    TOKEN_INVALID_MESSAGE,
+    get_jwt_token,
+)
 
 
 # TODO
@@ -29,3 +39,42 @@ def test_get_validator_manifest():
 @pytest.mark.skip()
 def test_get_auth():
     assert 1 == 1
+
+
+def test_get_jwt_token():
+    expiration = (datetime.datetime.now().timestamp() + 1000).__floor__()
+
+    jwk = jwk_from_dict(
+        {
+            "alg": "HS256",
+            "kty": "oct",
+            "kid": "050bf691-4348-4891-940f-99af8354e82b",
+            "k": "eCE35cBrbRsO1GhrbxLXnGrVATgUFZDrPyyuOar4crw",
+        }
+    )
+
+    valid_jwt = JWT().encode(
+        {
+            "exp": expiration,
+        },
+        jwk,
+        "HS256",
+    )
+    creds = {"token": valid_jwt}
+    assert get_jwt_token(Credentials.from_dict(creds)) == valid_jwt
+
+    with pytest.raises(Exception) as e:
+        expiration = (datetime.datetime.now().timestamp() - 1000).__floor__()
+        expired_jwt = JWT().encode(
+            {
+                "exp": expiration,
+            },
+            jwk,
+            "HS256",
+        )
+        get_jwt_token(Credentials.from_dict({"token": expired_jwt}))
+    assert str(e.value) == TOKEN_EXPIRED_MESSAGE
+
+    with pytest.raises(Exception) as e:
+        get_jwt_token(Credentials.from_dict({"token": "invalid_token"}))
+    assert str(e.value) == TOKEN_INVALID_MESSAGE
