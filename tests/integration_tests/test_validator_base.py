06f2e7144bf379a605e92f5c67b9a3284cfd28b0
Zayd Simjee <zaydsimjee@gmail.com>
2024-04-08 13:58:16 -0700

    lint fix

--- tests/integration_tests/test_validator_base.py
+++ tests/integration_tests/test_validator_base.py
@@ -1,20 +1,25 @@
 from typing import Any, Dict
+
 from guardrails.guard import Guard
-from guardrails.validator_base import FailResult, ValidationResult, Validator, register_validator
+from guardrails.validator_base import (
+    FailResult,
+    ValidationResult,
+    Validator,
+    register_validator,
+)
 
 
-@register_validator("failure", 'string')
+@register_validator("failure", "string")
 class FailureValidator(Validator):
     def validate(self, value: Any, metadata: Dict[str, Any]) -> ValidationResult:
         return FailResult(
-            error_message=(
-                "Failed cuz this is the failure validator"
-            ),
-            fix_value='FIXED',
+            error_message=("Failed cuz this is the failure validator"),
+            fix_value="FIXED",
         )
 
-def test_failure_mode():    
-    guard = Guard().use(FailureValidator, on_fail='fix')
-    res = guard.parse('hi')
-    assert res.validated_output == 'FIXED'
-    assert res.validation_passed # Should this even be true though?
+
+def test_failure_mode():
+    guard = Guard().use(FailureValidator, on_fail="fix")
+    res = guard.parse("hi")
+    assert res.validated_output == "FIXED"
+    assert res.validation_passed  # Should this even be true though?
