cd7e660c49c45af69e5cf006ae413e6579170cbb
Aarav Navani <38411399+oofmeister27@users.noreply.github.com>
2024-05-08 01:28:55 -0700

    add utils file and uninstall

---
+++ guardrails/cli/hub/uninstall.py
@@ -0,0 +1,83 @@
+import json
+import os
+import subprocess
+import sys
+from email.parser import BytesHeaderParser
+from string import Template
+from typing import List, Literal, Union
+
+import typer
+from pydash.strings import snake_case
+
+from guardrails.classes.generic import Stack
+from guardrails.cli.hub.hub import hub_command
+from guardrails.cli.logger import LEVELS, logger
+from guardrails.cli.server.hub_client import get_validator_manifest
+from guardrails.cli.server.module_manifest import ModuleManifest
+
+from utils import get_site_packages_location, get_org_and_package_dirs, update_file, get_hub_directory
+
+from .console import console
+
+def remove_from_hub_inits(manifest: ModuleManifest, site_packages: str):
+    org_package = get_org_and_package_dirs(manifest)
+    exports: List[str] = manifest.exports or []
+    sorted_exports = sorted(exports, reverse=True)
+    module_name = manifest.module_name
+    relative_path = ".".join([*org_package, module_name])
+    import_line = (
+        f"from guardrails.hub.{relative_path} import {', '.join(sorted_exports)}"
+    )
+
+    # Remove import line from main __init__.py
+    hub_init_location = os.path.join(site_packages, "guardrails", "hub", "__init__.py")
+    update_file(hub_init_location, import_line, remove=True)
+
+    # Remove import line from namespace __init__.py
+    namespace = org_package[0]
+    namespace_init_location = os.path.join(
+        site_packages, "guardrails", "hub", namespace, "__init__.py"
+    )
+    update_file(namespace_init_location, import_line, remove=True)
+
+def uninstall_hub_module(manifest: ModuleManifest, site_packages: str):
+    uninstall_directory = get_hub_directory(manifest, site_packages)
+    logger.info(f"Removing directory {uninstall_directory}")
+    subprocess.check_call(["rm", "-rf", uninstall_directory])
+
+
+@hub_command.command()
+def uninstall(
+    package_uri: str = typer.Argument(
+        help="URI to the package to uninstall. Example: hub://guardrails/regex_match."
+    ),
+):
+    """Uninstall a validator from the Hub."""
+    if not package_uri.startswith("hub://"):
+        logger.error("Invalid URI!")
+        sys.exit(1)
+
+    console.print(f"\nUninstalling {package_uri}...\n")
+    logger.log(
+        level=LEVELS.get("SPAM"),
+        msg=f"Uninstalling {package_uri}...",
+    )
+
+    # Validation
+    module_name = package_uri.replace("hub://", "")
+
+    # Prep
+    with console.status("Fetching manifest", spinner="bouncingBar"):
+        module_manifest = get_validator_manifest(module_name)
+        site_packages = get_site_packages_location()
+
+    # Uninstall
+    with console.status("Removing module", spinner="bouncingBar"):
+        uninstall_hub_module(module_manifest, site_packages)
+
+    # Cleanup
+    with console.status("Cleaning up", spinner="bouncingBar"):
+        remove_from_hub_inits(module_manifest, site_packages)
+
+    console.print("✅ Successfully uninstalled!")  # type: ignore
+    logger.log(level=LEVELS.get("SPAM"), msg="✅ Successfully uninstalled!")  # type: ignore
