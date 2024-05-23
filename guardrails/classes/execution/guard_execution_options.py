84c7f6cb94d5d58589eaa9f0f330397263cb4851
Caleb Courier <caleb.courier@gmail.com>
2024-05-22 19:07:00 -0500

    cleanup exec options

--- guardrails/classes/execution/guard_execution_options.py
+++ guardrails/classes/execution/guard_execution_options.py
@@ -6,9 +6,7 @@ from dataclasses import dataclass
 class GuardExecutionOptions:
     prompt: Optional[str] = None
     instructions: Optional[str] = None
+    msg_history: Optional[str] = None
     reask_prompt: Optional[str] = None
     reask_instructions: Optional[str] = None
     num_reasks: Optional[int] = None
-    prompt_schema: Optional[str] = None
-    instructions_schema: Optional[str] = None
-    msg_history_schema: Optional[str] = None
