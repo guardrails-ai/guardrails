0ed8563ceb3cc934ce17b7848f349d526552cecc
Aarav Navani <38411399+oofmeister27@users.noreply.github.com>
2024-06-03 16:38:11 -0700

    remove pytests for openai < 0

--- tests/unit_tests/test_llm_providers.py
+++ tests/unit_tests/test_llm_providers.py
@@ -20,24 +20,6 @@ from guardrails.utils.safe_get import safe_get_with_brackets
 
 from .mocks import MockAsyncOpenAILlm, MockOpenAILlm
 
-
-@pytest.mark.skipif(not OPENAI_VERSION.startswith("0"), reason="OpenAI v0 only")
-def test_openai_callable_does_not_retry_on_non_retryable_errors(mocker):
-    with pytest.raises(Exception) as e:
-        llm = MockOpenAILlm()
-        fail_non_retryable_spy = mocker.spy(llm, "fail_non_retryable")
-
-        arbitrary_callable = ArbitraryCallable(llm.fail_retryable, prompt="Hello")
-        arbitrary_callable()
-
-        assert fail_non_retryable_spy.call_count == 1
-        assert isinstance(e, PromptCallableException) is True
-        assert (
-            str(e)
-            == "The callable `fn` passed to `Guard(fn, ...)` failed with the following error: `Non-Retryable Error!`. Make sure that `fn` can be called as a function that takes in a single prompt string and returns a string."  # noqa
-        )
-
-
 def test_openai_callable_does_not_retry_on_success(mocker):
     llm = MockOpenAILlm()
     succeed_spy = mocker.spy(llm, "succeed")
@@ -50,25 +32,6 @@ def test_openai_callable_does_not_retry_on_success(mocker):
     assert response.output == "Hello world!"
     assert response.prompt_token_count is None
     assert response.response_token_count is None
-
-
-@pytest.mark.skipif(not OPENAI_VERSION.startswith("0"), reason="OpenAI v0 only")
-@pytest.mark.asyncio
-async def test_async_openai_callable_does_not_retry_on_non_retryable_errors(mocker):
-    with pytest.raises(Exception) as e:
-        llm = MockAsyncOpenAILlm()
-        fail_non_retryable_spy = mocker.spy(llm, "fail_non_retryable")
-
-        arbitrary_callable = AsyncArbitraryCallable(llm.fail_retryable, prompt="Hello")
-        await arbitrary_callable()
-
-        assert fail_non_retryable_spy.call_count == 1
-        assert isinstance(e, PromptCallableException) is True
-        assert (
-            str(e)
-            == "The callable `fn` passed to `Guard(fn, ...)` failed with the following error: `Non-Retryable Error!`. Make sure that `fn` can be called as a function that takes in a single prompt string and returns a string."  # noqa
-        )
-
 
 @pytest.mark.asyncio
 async def test_async_openai_callable_does_not_retry_on_success(mocker):
@@ -223,23 +186,6 @@ def test_openai_stream_callable(mocker, openai_stream_mock):
         assert actual_op == f"{i},"
         i += 1
 
-
-@pytest.mark.asyncio
-@pytest.mark.skipif(not OPENAI_VERSION.startswith("0"), reason="OpenAI v0 only")
-async def test_async_openai_callable(mocker, openai_mock):
-    mocker.patch("openai.Completion.acreate", return_value=openai_mock)
-
-    from guardrails.llm_providers import AsyncOpenAICallable
-
-    openai_callable = AsyncOpenAICallable()
-    response = await openai_callable(text="Hello")
-
-    assert isinstance(response, LLMResponse) is True
-    assert response.output == "Mocked LLM output"
-    assert response.prompt_token_count == 10
-    assert response.response_token_count == 20
-
-
 def test_openai_chat_callable(mocker, openai_chat_mock):
     mocker.patch(
         "openai.resources.chat.completions.Completions.create",
@@ -277,23 +223,6 @@ def test_openai_chat_stream_callable(mocker, openai_chat_stream_mock):
         assert actual_op == f"{i},"
         i += 1
 
-
-@pytest.mark.asyncio
-@pytest.mark.skipif(not OPENAI_VERSION.startswith("0"), reason="OpenAI v0 only")
-async def test_async_openai_chat_callable(mocker, openai_chat_mock):
-    mocker.patch("openai.ChatCompletion.acreate", return_value=openai_chat_mock)
-
-    from guardrails.llm_providers import AsyncOpenAIChatCallable
-
-    openai_chat_callable = AsyncOpenAIChatCallable()
-    response = await openai_chat_callable(text="Hello")
-
-    assert isinstance(response, LLMResponse) is True
-    assert response.output == "Mocked LLM output"
-    assert response.prompt_token_count == 10
-    assert response.response_token_count == 20
-
-
 def test_openai_chat_model_callable(mocker, openai_chat_mock):
     mocker.patch(
         "openai.resources.chat.completions.Completions.create",
@@ -315,48 +244,6 @@ def test_openai_chat_model_callable(mocker, openai_chat_mock):
     assert response.output == "Mocked LLM output"
     assert response.prompt_token_count == 10
     assert response.response_token_count == 20
-
-
-@pytest.mark.asyncio
-@pytest.mark.skipif(not OPENAI_VERSION.startswith("0"), reason="OpenAI v0 only")
-async def test_async_openai_chat_model_callable(mocker, openai_chat_mock):
-    mocker.patch("openai.ChatCompletion.acreate", return_value=openai_chat_mock)
-
-    from guardrails.llm_providers import AsyncOpenAIChatCallable
-
-    class MyModel(BaseModel):
-        a: str
-
-    openai_chat_model_callable = AsyncOpenAIChatCallable()
-    response = await openai_chat_model_callable(
-        text="Hello",
-        base_model=MyModel,
-    )
-
-    assert isinstance(response, LLMResponse) is True
-    assert response.output == "Mocked LLM output"
-    assert response.prompt_token_count == 10
-    assert response.response_token_count == 20
-
-
-@pytest.mark.skipif(
-    not importlib.util.find_spec("manifest"),
-    reason="manifest-ml is not installed",
-)
-def test_manifest_callable():
-    client = MagicMock()
-    client.run.return_value = "Hello world!"
-
-    from guardrails.llm_providers import ManifestCallable
-
-    manifest_callable = ManifestCallable()
-    response = manifest_callable(text="Hello", client=client)
-
-    assert isinstance(response, LLMResponse) is True
-    assert response.output == "Hello world!"
-    assert response.prompt_token_count is None
-    assert response.response_token_count is None
-
 
 @pytest.mark.skipif(
     not importlib.util.find_spec("manifest"),
