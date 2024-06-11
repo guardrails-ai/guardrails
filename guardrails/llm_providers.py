15a9a4823b42376cf0c9ba1c2439ff1fe079aae8
David Tam <david@guardrailsai.com>
2024-06-11 10:41:09 -0700

    update async too

--- guardrails/llm_providers.py
+++ guardrails/llm_providers.py
@@ -665,7 +665,7 @@ def get_llm_ask(llm_api: Callable, *args, **kwargs) -> PromptCallableBase:
     try:
         from litellm import completion  # noqa: F401 # type: ignore
 
-        if llm_api == completion or llm_api == None and kwargs.get("model"):
+        if llm_api == completion or (llm_api == None and kwargs.get("model")):
             return LiteLLMCallable(*args, **kwargs)
     except ImportError:
         pass
@@ -961,7 +961,7 @@ def get_async_llm_ask(
     try:
         import litellm
 
-        if llm_api == litellm.acompletion:
+        if llm_api == litellm.acompletion or (llm_api == None and kwargs.get("model")):
             return AsyncLiteLLMCallable(*args, **kwargs)
     except ImportError:
         pass
