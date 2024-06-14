0b188613a38a0ae6b96d16b645d79e323e9bc52e
David Tam <david@guardrailsai.com>
2024-06-14 14:13:52 -0700

    fix stream runner too

--- guardrails/run/async_stream_runner.py
+++ guardrails/run/async_stream_runner.py
@@ -18,6 +18,8 @@ from guardrails.llm_providers import (
     AsyncLiteLLMCallable,
     AsyncPromptCallableBase,
     LiteLLMCallable,
+    OpenAICallable,
+    OpenAIChatCallable,
     PromptCallableBase,
 )
 from guardrails.logger import set_scope
@@ -193,7 +195,31 @@ class AsyncStreamRunner(AsyncRunner, StreamRunner):
     def get_chunk_text(self, chunk: Any, api: Union[PromptCallableBase, None]) -> str:
         """Get the text from a chunk."""
         chunk_text = ""
-        if isinstance(api, LiteLLMCallable):
+        if isinstance(api, OpenAICallable):
+            if OPENAI_VERSION.startswith("0"):
+                finished = chunk["choices"][0]["finish_reason"]
+                if "text" in chunk["choices"][0]:
+                    content = chunk["choices"][0]["text"]
+                    if not finished and content:
+                        chunk_text = content
+            else:
+                finished = chunk.choices[0].finish_reason
+                content = chunk.choices[0].text
+                if not finished and content:
+                    chunk_text = content
+        elif isinstance(api, OpenAIChatCallable):
+            if OPENAI_VERSION.startswith("0"):
+                finished = chunk["choices"][0]["finish_reason"]
+                if "content" in chunk["choices"][0]["delta"]:
+                    content = chunk["choices"][0]["delta"]["content"]
+                    if not finished and content:
+                        chunk_text = content
+            else:
+                finished = chunk.choices[0].finish_reason
+                content = chunk.choices[0].delta.content
+                if not finished and content:
+                    chunk_text = content
+        elif isinstance(api, LiteLLMCallable):
             finished = chunk.choices[0].finish_reason
             content = chunk.choices[0].delta.content
             if not finished and content:
