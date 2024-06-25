57b71c12b620049dcdaf376e96a5ad6c0332fd3c
Caleb Courier <caleb.courier@gmail.com>
2024-06-24 19:06:49 -0500

    auto format

--- guardrails/formatters/base_formatter.py
+++ guardrails/formatters/base_formatter.py
@@ -7,9 +7,12 @@ from guardrails.llm_providers import (
 
 
 class BaseFormatter(ABC):
-    """A Formatter takes an LLM Callable and wraps the method into an abstract callable.
-    Used to perform manipulations of the input or the output, like JSON constrained-
-    decoding."""
+    """A Formatter takes an LLM Callable and wraps the method into an abstract
+    callable.
+
+    Used to perform manipulations of the input or the output, like JSON
+    constrained- decoding.
+    """
 
     @abstractmethod
     def wrap_callable(self, llm_callable: PromptCallableBase) -> ArbitraryCallable: ...
