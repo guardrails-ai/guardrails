bf5c8d865f462f29824b520d1c26ace625e7a479
Joseph Catrambone <joseph@guardrailsai.com>
2024-06-27 15:03:01 -0700

    Remove the macarena from unit tests.

--- tests/unit_tests/test_guard_log.py
+++ tests/unit_tests/test_guard_log.py
@@ -13,7 +13,6 @@ STOCK_MESSAGES = [
     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "Ut enim ad minim veniam",
     "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    "Heeeey Macarena.",
     "Excepteur sint occaecat cupidatat non proident,",
     "sunt in culpa qui officia deserunt mollit anim id est laborum.",
 ]
