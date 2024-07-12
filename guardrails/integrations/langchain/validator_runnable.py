c94d5c752bf77ff745f2efb27af607a3a039c201
Kaushik Bokka <kaushikbokka@gmail.com>
2024-07-12 18:53:36 +0530

    feat(langchain): Add LangSmith support for Runnables

--- guardrails/integrations/langchain/validator_runnable.py
+++ guardrails/integrations/langchain/validator_runnable.py
@@ -1,34 +1,18 @@
-from copy import deepcopy
-from typing import Optional, cast
-from langchain_core.messages import BaseMessage
-from langchain_core.runnables import Runnable, RunnableConfig
-from guardrails.classes.input_type import InputType
-from guardrails.errors import ValidationError
+from guardrails.integrations.langchain.base_runnable import BaseRunnable
 from guardrails.validator_base import FailResult, Validator
+from guardrails.errors import ValidationError
+from guardrails.classes.output_type import OT
 
 
-class ValidatorRunnable(Runnable):
+class ValidatorRunnable(BaseRunnable):
     validator: Validator
 
     def __init__(self, validator: Validator):
         self.name = validator.rail_alias
         self.validator = validator
 
-    def invoke(
-        self, input: InputType, config: Optional[RunnableConfig] = None
-    ) -> InputType:
-        output = BaseMessage(content="", type="")
-        str_input = None
-        input_is_chat_message = False
-        if isinstance(input, BaseMessage):
-            input_is_chat_message = True
-            str_input = str(input.content)
-            output = deepcopy(input)
-        else:
-            str_input = str(input)
-
-        response = self.validator.validate(str_input, self.validator._metadata)
-
+    def _validate(self, input: str) -> OT:
+        response = self.validator.validate(input, self.validator._metadata)
         if isinstance(response, FailResult):
             raise ValidationError(
                 (
@@ -36,8 +20,4 @@ class ValidatorRunnable(Runnable):
                     f" {response.error_message}"
                 )
             )
-
-        if input_is_chat_message:
-            output.content = str_input
-            return cast(InputType, output)
-        return cast(InputType, str_input)
+        return input
