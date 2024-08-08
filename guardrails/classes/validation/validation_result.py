26014a1ad401af3afa01b09101686cb385ef8a63
Joseph Catrambone <joseph@guardrailsai.com>
2024-08-08 12:36:26 -0700

    Make an explicit constructor for FailResult so that we don't get a footgun late in the game.

--- guardrails/classes/validation/validation_result.py
+++ guardrails/classes/validation/validation_result.py
@@ -116,6 +116,12 @@ class FailResult(ValidationResult, IFailResult):
     """
     error_spans: Optional[List["ErrorSpan"]] = None
 
+    def __init__(self, error_message: str, **kwargs) -> None:
+        super().__init__(error_message=error_message, **kwargs)
+        # This is a silly thing to force a friendly error message and to give type hints
+        # to IDEs who have a hard time figuring out the constructor parameters.
+        self.error_message = error_message
+
     @classmethod
     def from_interface(cls, i_fail_result: IFailResult) -> "FailResult":
         error_spans = None
