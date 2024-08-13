c1d785bb2a94ef02ab74190321990e290069981a
Aarav Navani <arav.navani@gmail.com>
2024-08-13 09:51:30 -0700

    remove added file

--- config.py
+++
@@ -1,6 +0,0 @@
-from guardrails import Guard
-from guardrails.hub import ToxicLanguage
-guard = Guard()
-guard.name = 'toxic_language_guard'
-print("GUARD PARAMETERS UNFILLED! UPDATE THIS FILE!")  # TODO: Remove this when parameters are filled.
-guard.use(ToxicLanguage())  # TODO: Add parameters.
