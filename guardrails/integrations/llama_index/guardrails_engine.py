80d53a585f08c55f447c32e198dd638832356cd8
Kaushik Bokka <kaushikbokka@gmail.com>
2024-08-27 16:42:32 +0530

    fix(2/n) pyright typing issues

--- guardrails/integrations/llama_index/guardrails_engine.py
+++ guardrails/integrations/llama_index/guardrails_engine.py
@@ -1,19 +1,51 @@
-from typing import Any, Optional, Dict, List, Union, TYPE_CHECKING
+from typing import Any, Optional, Dict, List, Union, TYPE_CHECKING, cast
 from guardrails import Guard
 from guardrails.errors import ValidationError
+from guardrails.classes.validation_outcome import ValidationOutcome
 
-if TYPE_CHECKING:
+try:
     from llama_index.core.query_engine import BaseQueryEngine
     from llama_index.core.chat_engine.types import BaseChatEngine
     from llama_index.core.schema import QueryBundle
     from llama_index.core.callbacks import CallbackManager
-    from llama_index.core.base.response.schema import RESPONSE_TYPE
-    from llama_index.core.base.llms.types import ChatMessage, ChatResponse
+    from llama_index.core.base.response.schema import (
+        RESPONSE_TYPE,
+        Response,
+        StreamingResponse,
+        AsyncStreamingResponse,
+        PydanticResponse,
+    )
+    from llama_index.core.base.llms.types import ChatMessage
     from llama_index.core.prompts.mixin import PromptMixinType
-else:
-    BaseQueryEngine = BaseChatEngine = QueryBundle = CallbackManager = RESPONSE_TYPE = (
-        ChatMessage
-    ) = ChatResponse = PromptMixinType = Any
+    from llama_index.core.chat_engine.types import (
+        AGENT_CHAT_RESPONSE_TYPE,
+        AgentChatResponse,
+        StreamingAgentChatResponse,
+    )
+except ImportError:
+    if TYPE_CHECKING:
+        from llama_index.core.query_engine import BaseQueryEngine
+        from llama_index.core.chat_engine.types import (
+            BaseChatEngine,
+            AGENT_CHAT_RESPONSE_TYPE,
+            AgentChatResponse,
+            StreamingAgentChatResponse,
+        )
+        from llama_index.core.schema import QueryBundle
+        from llama_index.core.callbacks import CallbackManager
+        from llama_index.core.base.response.schema import (
+            RESPONSE_TYPE,
+            Response,
+            StreamingResponse,
+            AsyncStreamingResponse,
+            PydanticResponse,
+        )
+        from llama_index.core.base.llms.types import ChatMessage
+        from llama_index.core.prompts.mixin import PromptMixinType
+    else:
+        BaseQueryEngine = BaseChatEngine = CallbackManager = Any
+        RESPONSE_TYPE = Response = AGENT_CHAT_RESPONSE_TYPE = PydanticResponse = Any
+        QueryBundle = ChatMessage = PromptMixinType = Any
 
 
 class GuardrailsEngine(BaseQueryEngine, BaseChatEngine):
@@ -67,9 +99,36 @@ class GuardrailsEngine(BaseQueryEngine, BaseChatEngine):
                 prompt=query_bundle.query_str,
                 **self._guard_kwargs,
             )
+
+            validated_output = cast(ValidationOutcome, validated_output)
+            self._engine_response = cast(RESPONSE_TYPE, self._engine_response)
+
+            if not validated_output.validation_passed:
+                raise ValidationError(f"Validation failed: {validated_output.error}")
             self._update_response_metadata(validated_output)
             if validated_output.validation_passed:
-                self._engine_response.response = validated_output.validated_output
+                if isinstance(self._engine_response, Response):
+                    self._engine_response.response = validated_output.validated_output
+                elif isinstance(
+                    self._engine_response, (StreamingResponse, AsyncStreamingResponse)
+                ):
+                    self._engine_response.response_txt = (
+                        validated_output.validated_output
+                    )
+                elif isinstance(self._engine_response, PydanticResponse):
+                    if self._engine_response.response:
+                        import json
+
+                        json_str = (
+                            validated_output.validated_output
+                            if isinstance(validated_output.validated_output, str)
+                            else json.dumps(validated_output.validated_output)
+                        )
+                        self._engine_response.response = self._engine_response.response.__class__.model_validate_json(  # noqa: E501
+                            json_str
+                        )
+                else:
+                    raise ValueError("Unsupported response type")
         except ValidationError as e:
             raise ValidationError(f"Validation failed: {str(e)}")
         except Exception as e:
@@ -78,7 +137,7 @@ class GuardrailsEngine(BaseQueryEngine, BaseChatEngine):
 
     def chat(
         self, message: str, chat_history: Optional[List[ChatMessage]] = None
-    ) -> ChatResponse:
+    ) -> AGENT_CHAT_RESPONSE_TYPE:
         if not isinstance(self._engine, BaseChatEngine):
             raise ValueError(
                 "Cannot perform chat with a QueryEngine. Use query() method instead."
@@ -93,38 +152,59 @@ class GuardrailsEngine(BaseQueryEngine, BaseChatEngine):
                 **self._guard_kwargs,
             )
             response = self._create_chat_response(validated_output)
+            if response is None:
+                raise ValueError("Failed to create a valid chat response")
+
+            return cast(AGENT_CHAT_RESPONSE_TYPE, response)
         except ValidationError as e:
             raise ValidationError(f"Validation failed: {str(e)}")
         except Exception as e:
             raise RuntimeError(f"An error occurred during chat processing: {str(e)}")
-        return response
 
     def _update_response_metadata(self, validated_output):
-        self._engine_response.metadata.update(
-            {
-                "validation_passed": validated_output.validation_passed,
-                "validated_output": validated_output.validated_output,
-                "error": validated_output.error,
-                "raw_llm_output": validated_output.raw_llm_output,
-            }
-        )
+        if self._engine_response is None:
+            return
+        self._engine_response = cast(RESPONSE_TYPE, self._engine_response)
 
-    def _create_chat_response(self, validated_output):
+        metadata_update = {
+            "validation_passed": validated_output.validation_passed,
+            "validated_output": validated_output.validated_output,
+            "error": validated_output.error,
+            "raw_llm_output": validated_output.raw_llm_output,
+        }
+
+        if self._engine_response.metadata is None:
+            self._engine_response.metadata = {}
+        self._engine_response.metadata.update(metadata_update)
+
+    def _create_chat_response(
+        self, validated_output
+    ) -> Optional[AGENT_CHAT_RESPONSE_TYPE]:
         if validated_output.validation_passed:
             content = validated_output.validated_output
         else:
             content = "I'm sorry, but I couldn't generate a valid response."
 
-        response = ChatResponse(
-            message=ChatMessage(role="assistant", content=content),
-            additional_kwargs={
-                "validation_passed": validated_output.validation_passed,
-                "validated_output": validated_output.validated_output,
-                "error": validated_output.error,
-                "raw_llm_output": validated_output.raw_llm_output,
-            },
-        )
-        return response
+        if self._engine_response is None:
+            return None
+
+        metadata_update = {
+            "validation_passed": validated_output.validation_passed,
+            "validated_output": validated_output.validated_output,
+            "error": validated_output.error,
+            "raw_llm_output": validated_output.raw_llm_output,
+        }
+
+        if isinstance(self._engine_response, AgentChatResponse):
+            if self._engine_response.metadata is None:
+                self._engine_response.metadata = {}
+            self._engine_response.metadata.update(metadata_update)
+        elif isinstance(self._engine_response, StreamingAgentChatResponse):
+            for key, value in metadata_update.items():
+                setattr(self._engine_response, key, value)
+
+        self._engine_response.response = content
+        return self._engine_response
 
     async def _aquery(self, query_bundle: QueryBundle) -> RESPONSE_TYPE:
         """Async version of _query."""
@@ -134,17 +214,25 @@ class GuardrailsEngine(BaseQueryEngine, BaseChatEngine):
         self, message: str, chat_history: Optional[List[ChatMessage]] = None
     ):
         """Async version of chat."""
-        raise NotImplementedError("Async chat is not supported with guardrails.")
+        raise NotImplementedError(
+            "Async chat is not supported in the GuardrailsQueryEngine."
+        )
 
     def stream_chat(
         self, message: str, chat_history: Optional[List[ChatMessage]] = None
     ):
-        raise NotImplementedError("Stream chat is not supported with guardrails.")
+        """Stream chat responses."""
+        raise NotImplementedError(
+            "Stream chat is not supported in the GuardrailsQueryEngine."
+        )
 
     async def astream_chat(
         self, message: str, chat_history: Optional[List[ChatMessage]] = None
     ):
-        raise NotImplementedError("Async stream chat is not supported with guardrails.")
+        """Async stream chat responses."""
+        raise NotImplementedError(
+            "Async stream chat is not supported in the GuardrailsQueryEngine."
+        )
 
     def reset(self):
         """Reset the chat history."""
