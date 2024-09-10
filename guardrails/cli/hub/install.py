ed791ccaae2898f5d894ec108ea0544e6e4f1479
Aarav Navani <arav.navani@gmail.com>
2024-09-09 22:11:03 -0700

    install multiple validators

--- guardrails/cli/hub/install.py
+++ guardrails/cli/hub/install.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Optional
+from typing import Optional, List
 
 import typer
 
@@ -10,9 +10,9 @@ from guardrails.cli.telemetry import trace_if_enabled
 
 @hub_command.command()
 def install(
-    package_uri: str = typer.Argument(
-        help="URI to the package to install.\
-Example: hub://guardrails/regex_match."
+    package_uris: List[str] = typer.Argument(
+        ...,
+        help="URIs to the packages to install. Example: hub://guardrails/regex_match hub://guardrails/spell_check",
     ),
     local_models: Optional[bool] = typer.Option(
         None,
@@ -28,17 +28,16 @@ Example: hub://guardrails/regex_match."
 ):
     try:
         trace_if_enabled("hub/install")
-        from guardrails.hub.install import install
+        from guardrails.hub.install import install_multiple
 
         def confirm():
             return typer.confirm(
                 "This validator has a Guardrails AI inference endpoint available. "
-                "Would you still like to install the"
-                " local models for local inference?",
+                "Would you still like to install the local models for local inference?",
             )
 
-        install(
-            package_uri,
+        install_multiple(
+            package_uris,
             install_local_models=local_models,
             quiet=quiet,
             install_local_models_confirm=confirm,
