59f2be50b4978e7e15d47157d91ead712c3ebd0c
zsimjee <zaydsimjee@gmail.com>
2024-09-18 12:50:38 -0700

    wip

--- docs/integrations/nemo_guardrails/nm_gr.py
+++ docs/integrations/nemo_guardrails/nm_gr.py
@@ -1,7 +1,11 @@
+from typing import List
 from nemoguardrails import LLMRails, RailsConfig
+from guardrails import Guard
 from guardrails.integrations.nemoguardrails.nemoguardrails_guard import (
     NemoguardrailsGuard,
 )
+from pydantic import BaseModel, Field
+import rich
 
 # install("hub://guardrails/toxic_language", install_local_models=False)
 from guardrails.hub import ToxicLanguage  # noqa: E402
@@ -13,16 +17,175 @@ nemorails = LLMRails(config)
 # define your guard and validator(s)
 guard = NemoguardrailsGuard(nemorails=nemorails).use(ToxicLanguage())
 
-res = guard(
-    messages=[
-        {
-            "role": "user",
-            "content": """Generate a list of words that I should ban at work.
-        Make sure to spell out all the words. Return only the list of words without
-        preamble or postamble. Make sure to return ONLY the list of words.
-        This list should include profanity, slurs, and other offensive language.""",
-        }
-    ]
+# res = guard(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": """Generate a list of words that I should ban at work.
+#         Make sure to spell out all the words. Return only the list of words without
+#         preamble or postamble. Make sure to return ONLY the list of words.
+#         This list should include profanity, slurs, and other offensive language.""",
+#         }
+#     ],
+# )
+
+# print(res.validation_passed)  # type: ignore
+
+# rich.print(guard.history.last.tree)
+
+# res = guard(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": """Say hi in the nicest way you can""",
+#         }
+#     ]
+# )
+
+# print(res.validation_passed)  # type: ignore
+
+class StructuredOutput(BaseModel):
+    nontoxic: str = Field(validators=[ToxicLanguage()])
+
+# pydantic_guard = NemoguardrailsGuard.from_pydantic(
+#     nemorails=nemorails,
+#     output_class=StructuredOutput,
+# )
+
+# res = pydantic_guard(
+#     messages=[
+#         {
+#             "role": "system",
+#             "content": """Only respond in JSON. The JSON should be formatted as follows:
+            
+#             {'nontoxic': response to the prompt}
+            
+#             AGAIN, be sure to respond ONLY with valid JSON. Do not include any other text.
+#             """,
+#         },
+#         {
+#             "role": "user",
+#             "content": """Generate a list of words that I should ban at work.
+#         Make sure to spell out all the words. Return only the list of words without
+#         preamble or postamble. Make sure to return ONLY the list of words.
+#         This list should include profanity, slurs, and other offensive language.""",
+#         }
+#     ],
+#     num_reasks=1
+# )
+
+# print(res.validation_passed)  # type: ignore
+
+# rich.print(pydantic_guard.history.last.tree)
+
+
+
+# class StructuredOutput(BaseModel):
+#     nontoxic: str = Field(validators=[ToxicLanguage()])
+
+# pydantic_guard = Guard.from_pydantic(
+#     # nemorails=nemorails,
+#     output_class=StructuredOutput,
+# )
+
+# g = Guard()
+# g(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         # {
+#         #     "role": "system",
+#         #     "content": """Only respond in JSON. The JSON should be formatted as follows:
+            
+#         #     {'nontoxic': response to the prompt}
+            
+#         #     AGAIN, be sure to respond ONLY with valid JSON. Do not include any other text.
+#         #     """,
+#         # },
+#         {
+#             "role": "user",
+#             "content": """Write  pydantic structure about medical data. It should have hundreds of fields, with some nesting""",
+#         }
+#     ],
+#     # tools=pydantic_guard.json_function_calling_tool(),
+#     # tool_choice="required",
+#     # generate_kwargs={"temperature": 0.0},
+# )
+# # rich.print(pydantic_guard.history.last.tree)
+# rich.print(g.history.last.tree)
+# print(g.history.last.raw_outputs)
+
+
+class MedicalData(BaseModel):
+    patient_id: int
+    patient_name: str
+    age: int
+    gender: str
+    address: str
+    contact_number: str
+    blood_type: str
+    allergies: List[str]
+    medications: List[str]
+    medical_history: List[str]
+    family_history: List[str]
+    insurance_provider: str
+    insurance_policy_number: str
+    emergency_contact_name: str
+    emergency_contact_number: str
+    primary_care_physician: str
+    last_checkup_date: str
+    next_checkup_date: str
+    weight: float
+    height: float
+    blood_pressure: str
+    heart_rate: int
+    respiratory_rate: int
+    temperature: float
+    symptoms: List[str]
+    diagnosis: str
+    treatment_plan: str
+    lab_results: List[str]
+    imaging_results: List[str]
+    follow_up_instructions: str
+    additional_notes: str
+    vaccination_records: List[str]
+    surgeries: List[str]
+    hospitalizations: List[str]
+    chronic_conditions: List[str]
+    lifestyle_factors: List[str]
+    exercise_routine: str
+    diet_plan: str
+    sleep_pattern: str
+    stress_level: str
+    smoking_status: str
+    alcohol_consumption: str
+    drug_usage: str
+    mental_health_history: List[str]
+    social_support_system: List[str]
+    living_situation: str
+    employment_status: str
+    education_level: str
+    income_level: str
+    access_to_healthcare: str
+    healthcare_preferences: List[str]
+    healthcare_goals: List[str]
+    healthcare_challenges: List[str]
+    healthcare_needs: List[str]
+    healthcare_preferences: List[str]
+    healthcare_expectations: List[str]
+    healthcare_satisfaction: str
+    healthcare_feedback: str
+    additional_information: str
+    # Add more fields as needed
+
+
+med_pydantic_guard = Guard.from_pydantic(
+    output_class=MedicalData,
 )
 
-print(res.validation_passed)  # type: ignore
+import time
+
+init_time = time.time()
+
+med_pydantic_guard.json_function_calling_tool()
+
+print(f"Time taken: {time.time() - init_time}")
--- guardrails/integrations/nemoguardrails/nemoguardrails_guard.py
+++ guardrails/integrations/nemoguardrails/nemoguardrails_guard.py
@@ -1,11 +1,22 @@
-from typing import Callable, Iterable, Optional, Union
+from typing import Callable, Dict, Iterable, List, Optional, Union, cast
+import warnings
 from typing_extensions import deprecated
 
-from guardrails.classes.output_type import OT
+from guardrails.classes.execution.guard_execution_options import GuardExecutionOptions
+from guardrails.classes.output_type import OT, OutputTypes
 from guardrails.classes.validation_outcome import ValidationOutcome
 
 from guardrails import Guard
 from nemoguardrails import LLMRails
+
+from guardrails.formatters import get_formatter
+from guardrails.formatters.base_formatter import BaseFormatter
+from guardrails.schema.pydantic_schema import pydantic_model_to_schema
+from guardrails.types.pydantic import ModelOrListOfModels
+
+from guardrails.stores.context import (
+    Tracer
+)
 
 
 class NemoguardrailsGuard(Guard):
@@ -19,7 +30,7 @@ class NemoguardrailsGuard(Guard):
         self._nemorails = nemorails
 
     def __call__(
-        self, llm_api: Optional[Callable] = None, *args, **kwargs
+        self, llm_api: Optional[Callable] = None, generate_kwargs: Optional[Dict] = None, *args, **kwargs
     ) -> Union[ValidationOutcome[OT], Iterable[ValidationOutcome[OT]]]:
         # peel llm_api off of kwargs
         llm_api = kwargs.pop("llm_api", None)
@@ -42,32 +53,150 @@ class NemoguardrailsGuard(Guard):
  dictionaries, where each dictionary has a 'role' key and a 'content' key."""
             )
 
-        # create the callable
-        def custom_callable(**kwargs):
-            # .generate doesn't like temp
-            kwargs.pop("temperature", None)
+        def _custom_nemo_callable(*args, **kwargs):
+            return self._custom_nemo_callable(*args, generate_kwargs=generate_kwargs, **kwargs)
 
-            msg_history = kwargs.pop("msg_history", None)
-            messages = (
-                msg_history
-                if kwargs.get("messages") is None
-                else kwargs.get("messages")
+        return super().__call__(llm_api=_custom_nemo_callable, *args, **kwargs)
+
+    @classmethod
+    def from_pydantic(
+        cls,
+        nemorails: LLMRails,
+        output_class: ModelOrListOfModels,
+        *,
+        prompt: Optional[str] = None,
+        instructions: Optional[str] = None,
+        num_reasks: Optional[int] = None,
+        reask_prompt: Optional[str] = None,
+        reask_instructions: Optional[str] = None,
+        reask_messages: Optional[List[Dict]] = None,
+        messages: Optional[List[Dict]] = None,
+        tracer: Optional[Tracer] = None,
+        name: Optional[str] = None,
+        description: Optional[str] = None,
+        output_formatter: Optional[Union[str, BaseFormatter]] = None,
+    ):
+        """Create a Guard instance using a Pydantic model to specify the output
+        schema.
+
+        Args:
+            output_class: (Union[Type[BaseModel], List[Type[BaseModel]]]): The pydantic model that describes
+            the desired structure of the output.
+            prompt (str, optional): The prompt used to generate the string. Defaults to None.
+            instructions (str, optional): Instructions for chat models. Defaults to None.
+            reask_prompt (str, optional): An alternative prompt to use during reasks. Defaults to None.
+            reask_instructions (str, optional): Alternative instructions to use during reasks. Defaults to None.
+            reask_messages (List[Dict], optional): A list of messages to use during reasks. Defaults to None.
+            num_reasks (int, optional): The max times to re-ask the LLM if validation fails. Deprecated
+            tracer (Tracer, optional): An OpenTelemetry tracer to use for metrics and traces. Defaults to None.
+            name (str, optional): A unique name for this Guard. Defaults to `gr-` + the object id.
+            description (str, optional): A description for this Guard. Defaults to None.
+            output_formatter (str | Formatter, optional): 'none' (default), 'jsonformer', or a Guardrails Formatter.
+        """  # noqa
+
+        if num_reasks:
+            warnings.warn(
+                "Setting num_reasks during initialization is deprecated"
+                " and will be removed in 0.6.x!"
+                "We recommend setting num_reasks when calling guard()"
+                " or guard.parse() instead."
+                "If you insist on setting it at the Guard level,"
+                " use 'Guard.configure()'.",
+                DeprecationWarning,
             )
 
-            prompt = kwargs.get("prompt")
+        if reask_instructions:
+            warnings.warn(
+                "reask_instructions is deprecated and will be removed in 0.6.x!"
+                "Please be prepared to set reask_messages instead.",
+                DeprecationWarning,
+            )
+        if reask_prompt:
+            warnings.warn(
+                "reask_prompt is deprecated and will be removed in 0.6.x!"
+                "Please be prepared to set reask_messages instead.",
+                DeprecationWarning,
+            )
 
-            if messages is not None:
-                kwargs["messages"] = messages
+        # We have to set the tracer in the ContextStore before the Rail,
+        #   and therefore the Validators, are initialized
+        cls._set_tracer(cls, tracer)  # type: ignore
 
-            if (messages is None) and (prompt is None):
-                raise ValueError("""messages or prompt should be passed.""")
+        schema = pydantic_model_to_schema(output_class)
+        exec_opts = GuardExecutionOptions(
+            prompt=prompt,
+            instructions=instructions,
+            reask_prompt=reask_prompt,
+            reask_instructions=reask_instructions,
+            reask_messages=reask_messages,
+            messages=messages,
+        )
 
-            return (self._nemorails.generate(**kwargs))["content"]  # type: ignore
+        # TODO: This is the only line that's changed vs the parent Guard class
+        # Find a way to refactor this
+        guard = cls(
+            nemorails=nemorails,
+            name=name,
+            description=description,
+            output_schema=schema.json_schema,
+            validators=schema.validators,
+        )
+        if schema.output_type == OutputTypes.LIST:
+            guard = cast(Guard[List], guard)
+        else:
+            guard = cast(Guard[Dict], guard)
+        guard.configure(num_reasks=num_reasks, tracer=tracer)
+        guard._validator_map = schema.validator_map
+        guard._exec_opts = exec_opts
+        guard._output_type = schema.output_type
+        guard._base_model = output_class
+        if isinstance(output_formatter, str):
+            if isinstance(output_class, list):
+                raise Exception("""Root-level arrays are not supported with the 
+                jsonformer argument, but can be used with other json generation methods.
+                Omit the output_formatter argument to use the other methods.""")
+            output_formatter = get_formatter(
+                output_formatter,
+                schema=output_class.model_json_schema(),  # type: ignore
+            )
+        guard._output_formatter = output_formatter
+        guard._fill_validators()
+        return guard
 
-        return super().__call__(llm_api=custom_callable, *args, **kwargs)
+    # create the callable
+    def _custom_nemo_callable(self, *args, generate_kwargs, **kwargs):
+        # .generate doesn't like temp
+        kwargs.pop("temperature", None)
 
-    def from_pydantic(self, *args, **kwargs):
-        pass
+        # msg_history, messages, prompt, and instruction all may or may not be present.
+        # if none of them are present, raise an error
+        # if messages is present, use that
+        # if msg_history is present, use 
+
+        msg_history = kwargs.pop("msg_history", None)
+        messages = kwargs.pop("messages", None)
+        prompt = kwargs.pop("prompt", None)
+        instructions = kwargs.pop("instructions", None)
+        
+        if msg_history is not None and messages is None:
+            messages = msg_history
+
+        if messages is None and msg_history is None:
+            messages = []
+            if instructions is not None:
+                messages.append({"role": "system", "content": instructions})
+            if prompt is not None:
+                messages.append({"role": "system", "content": prompt})
+
+        if messages is [] or messages is None:
+            raise ValueError("messages, prompt, or instructions should be passed during a call.")
+        
+        # kwargs["messages"] = messages
+
+        # return (self._nemorails.generate(**kwargs))["content"]  # type: ignore
+        if not generate_kwargs:
+            generate_kwargs = {}
+        return (self._nemorails.generate(messages=messages, **generate_kwargs))["content"]  # type: ignore
 
     @deprecated(
         "This method has been deprecated. Please use the main constructor `NemoGuardrailsGuard(nemorails=nemorails)` or the `from_pydantic` method.",
@@ -75,5 +204,14 @@ class NemoguardrailsGuard(Guard):
     def from_rail_string(cls, *args, **kwargs):
         raise NotImplementedError("""\
 `from_rail_string` is not implemented for NemoguardrailsGuard.
+We recommend using the main constructor `NemoGuardrailsGuard(nemorails=nemorails)`
+or the `from_pydantic` method.""")
+
+    @deprecated(
+        "This method has been deprecated. Please use the main constructor `NemoGuardrailsGuard(nemorails=nemorails)` or the `from_pydantic` method.",
+    )
+    def from_rail(cls, *args, **kwargs):
+        raise NotImplementedError("""\
+`from_rail` is not implemented for NemoguardrailsGuard.
 We recommend using the main constructor `NemoGuardrailsGuard(nemorails=nemorails)`
 or the `from_pydantic` method.""")
